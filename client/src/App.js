f9a071b835167a0792018932e9e66d8d4a14bec7
Edema <edemaero@gmail.com>
2022-11-17 02:19:41 +0100

    feat: Footer Layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Footer from "./Layouts/Footer";
 
 function App() {
 	return (
 		<div className="bg-gray-200 text-2xl py-6 text-center">
-			App goes here
+			{/* App goes here */}
+			<Footer />
 		</div>
 	);
 }
