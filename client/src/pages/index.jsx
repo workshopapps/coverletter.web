f29c8290edc39412b9a74db8b8f235a239544685
HaroldObasi <haroldobasi2k16@gmail.com>
2022-11-18 00:46:00 +0100

    adedd pricing for export

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -3,5 +3,6 @@ import History from "./History";
 import ErrorPage from "./ErrorPage";
 import ContactUs from "./ContactUs";
 import Faq from "./Faq";
+import Pricing from "./Pricing";
 
-export { Home, History, ErrorPage, ContactUs, Faq };
+export { Home, History, ErrorPage, ContactUs, Faq, Pricing };
