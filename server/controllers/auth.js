b9e008e4ade58ee0a698c2f9fe5c5653b2450edd
Muhammad <muhammad17gb@gmail.com>
2022-11-17 19:56:08 +0100

    start otp

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,38 +1,39 @@
-const User = require('../models/User')
-const { StatusCodes } = require('http-status-codes');
-const {randomString} = require('../utils/randomString')
-const { BadRequestError } = require('../errors')
-const sendEmail = require('../utils/sendEmail')
-
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const { randomString } = require("../utils/randomString");
+const { BadRequestError } = require("../errors");
+const sendEmail = require("../utils/sendEmail");
 
 const register = async (req, res) => {
-    const { email, name, password } = req.body;
-    
-    const oldUser = await User.findOne({ email });
-   
-    if (oldUser) {
-      
-      throw new BadRequestError('Email already exists');
-    }
-    let confirmationCode = randomString()
-    console.log(confirmationCode)
-   const user = new User({
-      name,
-      email,
-      password,
-      confirmationCode
-  },)
-  const url = `${process.env.BASE_URL}/auth/verify/${confirmationCode}`;
-  await sendEmail(req.body.email,"Verify email",url)
+	const { email, name, password } = req.body;
 
-   const savedUser = await user.save()
-    const token = savedUser.createJWT() 
-    
-  
-   
-    res.status(StatusCodes.CREATED).json({ user: token });
-  };
+	const oldUser = await User.findOne({ email });
 
-  module.exports = {
-    register
-  }
+	if (oldUser) {
+		throw new BadRequestError("Email already exists");
+	}
+	let confirmationCode = randomString();
+	console.log(confirmationCode);
+	const user = new User({
+		name,
+		email,
+		password,
+		confirmationCode,
+	});
+	const url = `${process.env.BASE_URL}/auth/verify/${confirmationCode}`;
+	await sendEmail(req.body.email, "Verify email", url);
+
+	const savedUser = await user.save();
+	const token = savedUser.createJWT();
+
+	res.status(StatusCodes.CREATED).json({ user: token });
+};
+
+const validateOtp = async (req, res, next) => {
+	const { otp } = req.body;
+	console.log(otp);
+};
+module.exports = {
+	register,
+	validateOtp,
+};
