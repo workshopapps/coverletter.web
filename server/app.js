7565ac5840d1c95ae44937fe900ec5aa202a2c39
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-11-17 16:37:42 +0100

    Added Initial Setup for Swagger Documentation

--- server/app.js
+++ server/app.js
@@ -2,14 +2,49 @@ require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
 const routes = require("./routes/index");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
+
+const options = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "Cover Letter Generator",
+			version: "1.0.0",
+			description: "API documentation for a Cover letter generator",
+		},
+		servers: [
+			{
+				url: "http://localhost:5001",
+				description: "Local",
+			},
+			// {
+			// 	url: "http://www.aplicar.herokuapp",
+			// 	description: "Development",
+			// },
+			// {
+			// 	url: "http://www.aplicar.com",
+			// 	description: "Production",
+			// },
+		],
+	},
+	apis: ["./server/routes/*.js"],
+};
+
+const openapiSpecification = swaggerJsDoc(options);
+const port = process.env.PORT || 5001;
+
 const app = express();
+
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(openapiSpecification));
 
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+
 
 app.use(express.json());
 app.use(helmet());
@@ -25,9 +60,6 @@ app.get("/", (req, res) => {
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
-
-const port = process.env.PORT || 5000;
-
 const start = async () => {
 	try {
 		//connect DB
