8f0cd793b207039c2b0040ad1283b1d1bcd408ae
Temitope Agbaje <agbajeinioluwa@gmail.com>
2022-11-17 22:48:44 +0100

    feat: Added Sign up test case

--- server/models/User.js
+++ server/models/User.js
@@ -1,70 +1,74 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Please provide name'],
-    maxlength: 50,
-    minlength: 3,
-  },
-  email: {
-    type: String,
-    required: [true, 'Please provide email'],
-    match: [
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      'Please provide a valid email',
-    ],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, 'Please provide password'],
-    minlength: 6,
-  },
-  status: {
-    type: String,
-    enum: ['Pending', 'Active'],
-    default: 'Pending'
-  },
-  confirmationCode: {
-    type: String,
-    unique: true
-  },
-  resetPasswordOtp: {
-    type: String,
-  }
-})
+const UserSchema = mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, "Please provide name"],
+		maxlength: 50,
+		minlength: 3,
+	},
+	email: {
+		type: String,
+		required: [true, "Please provide email"],
+		match: [
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+			"Please provide a valid email",
+		],
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: [true, "Please provide password"],
+		minlength: 6,
+	},
+	status: {
+		type: String,
+		enum: ["Pending", "Active"],
+		default: "Pending",
+	},
+	confirmationCode: {
+		type: String,
+		unique: true,
+	},
+	resetPasswordOtp: {
+		type: String,
+	},
+});
 
-UserSchema.pre('save', async function () {
-  const salt = await bcrypt.genSalt(10)
-  this.password = await bcrypt.hash(this.password, salt)
-})
+UserSchema.pre("save", async function () {
+	const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password, salt);
+});
 
 UserSchema.methods.createJWT = function () {
-  return jwt.sign(
-    { userId: this._id, name: this.name, email: this.email, status: this.status },
-    "secretkey123",
-    {
-      expiresIn: "5h",
-    }
-  )
-}
-
-UserSchema.methods.comparePassword = async function (canditatePassword) {
-  const isMatch = await bcrypt.compare(canditatePassword, this.password)
-  return isMatch
-}
-
-
-userSchema.methods.createPasswordResetToken = function () {
-
-  const otp = Math.floor(1000 + Math.random() * 9000).toString()
-  this.resetPasswordOtp = crypto.createHash('sha256').update(otp).digest('hex');
-  return otp;
+	return jwt.sign(
+		{
+			userId: this._id,
+			name: this.name,
+			email: this.email,
+			status: this.status,
+		},
+		"secretkey123",
+		{
+			expiresIn: "5h",
+		}
+	);
 };
 
+UserSchema.methods.comparePassword = async function (canditatePassword) {
+	const isMatch = await bcrypt.compare(canditatePassword, this.password);
+	return isMatch;
+};
 
+UserSchema.methods.createPasswordResetToken = function () {
+	const otp = Math.floor(1000 + Math.random() * 9000).toString();
+	this.resetPasswordOtp = crypto
+		.createHash("sha256")
+		.update(otp)
+		.digest("hex");
+	return otp;
+};
 
-module.exports = mongoose.model('User', UserSchema)
+module.exports = mongoose.model("User", UserSchema);
