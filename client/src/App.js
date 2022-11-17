9f4b02015f306bd96b8f9b4aedabc5be3f703828
Balikis Oyeleye <96819668+Qreamville@users.noreply.github.com>
2022-11-17 07:59:41 -1200

    Revert "Feature/Faq"

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Home, ErrorPage, ContactUs, Faq } from "./pages";
+import { Home, ErrorPage, ContactUs } from "./pages";
 import { ScrollToTop } from "./Components";
 import { Header, Footer } from "./Layouts";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -12,7 +12,7 @@ const App = () => {
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/contactus" element={<ContactUs />} />
-					<Route path="/faq" element={<Faq />} />
+
 					<Route path="*" element={<ErrorPage />}></Route>
 				</Routes>
 				<Footer />
--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -2,6 +2,5 @@ import Home from "./Home";
 import History from "./History";
 import ErrorPage from "./ErrorPage";
 import ContactUs from "./ContactUs";
-import Faq from "./Faq";
 
-export { Home, History, ErrorPage, ContactUs, Faq };
+export { Home, History, ErrorPage, ContactUs };
