5d987c7b9623a32cff3bac9db41e7374db8379dd
Edema <edemaero@gmail.com>
2022-11-18 15:41:22 +0100

    feat(FE-16): added career page

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -3,5 +3,6 @@ import History from "./History";
 import ErrorPage from "./ErrorPage";
 import ContactUs from "./ContactUs";
 import Faq from "./Faq";
+import Career from "./Career";
 
-export { Home, History, ErrorPage, ContactUs, Faq };
+export { Home, History, ErrorPage, ContactUs, Faq, Career };
