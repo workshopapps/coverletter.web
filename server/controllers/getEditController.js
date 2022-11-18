12d852377420fa685f7bd9c3cf13a59623f54118
Toluwalase <toluwalase@Toluwalases-MacBook-Air.local>
2022-11-18 20:46:06 +0000

    feat(BE-7)-: added endpoint to get all cvs created by a user

--- server/controllers/getEditController.js
+++ server/controllers/getEditController.js
@@ -30,6 +30,43 @@ const getAConvertLetter = async (req, res) => {
 	}
 };
 
+/**
+ * @desc It gets all the convert Letters created by a user
+ *
+ * @param {object} req
+ * @param {object} res
+ *
+ * @returns {object} the object
+ */
+
+const getAllConvertLettersByAUser = async (req, res) => {
+	try {
+		const template = await Template.find({}).exec();
+
+		if (!template) {
+			return res.status(404).json({
+				error: "Template does not exist",
+			});
+		}
+
+		return res.status(200).json({
+			message: "Template requested successfully",
+			data: template,
+		});
+	} catch (err) {
+		return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+	}
+};
+
+/**
+ * @desc It edits a convert Letter
+ *
+ * @param {object} req
+ * @param {object} res
+ *
+ * @returns {object} the result
+ */
+
 const editACovertLetter = async (req, res) => {
 	try {
 		const { id } = req.params;
@@ -54,7 +91,8 @@ const editACovertLetter = async (req, res) => {
 	}
 };
 
-module.exports = { 
-    getAConvertLetter,
-    editACovertLetter
-}
+module.exports = {
+	getAConvertLetter,
+	getAllConvertLettersByAUser,
+	editACovertLetter,
+};
