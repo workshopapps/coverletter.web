02ccde36213b51d609d0bb443409fa3a6a86a0f0
Toluwalase <toluwalase@Toluwalases-MacBook-Air.local>
2022-11-18 19:48:01 +0000

    feat(BE-7): added endpoints for the coverletter

---
+++ server/controllers/getEditController.js
@@ -0,0 +1,60 @@
+const { StatusCodes } = require("http-status-codes");
+
+const User = require("../models/User");
+const Template = require("../models/Template");
+
+const { BadRequestError } = require("../errors");
+
+const getAConvertLetter = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const isTemplateIdValid = !!id;
+		if (!isTemplateIdValid) {
+			throw new BadRequestError("Invalid template ID");
+		}
+
+		const template = await Template.findById(id).exec();
+
+		if (!template) {
+			return res.status(404).json({
+				error: "Template does not exist",
+			});
+		}
+
+		return res.status(200).json({
+			message: "Template requested successfully",
+			data: template,
+		});
+	} catch (err) {
+		return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+	}
+};
+
+const editACovertLetter = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const { template } = req.body;
+		const isTemplateIdValid = !!id || template;
+
+		if (!isTemplateIdValid) {
+			throw new BadRequestError("Invalid template request");
+		}
+
+		const editedTemplate = await Template.update(
+			{ _id: id },
+			{ $set: { template: template } }
+		).exec();
+
+		return res.status(200).json({
+			message: "Template edited successfully",
+			data: editedTemplate,
+		});
+	} catch (err) {
+		return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+	}
+};
+
+module.exports = { 
+    getAConvertLetter,
+    editACovertLetter
+}
