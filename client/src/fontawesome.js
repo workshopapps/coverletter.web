6efb4c3af2d563df00468f4ef0d75f6cf343b00c
Samuel Seibidor <samrythmn@gmail.com>
2022-11-18 12:31:46 +0100

    feat: installed font awesome icon library
    Installed and configured the font awesome icon library. I also added the basic fonts that would be used in the app

---
+++ client/src/fontawesome.js
@@ -0,0 +1,20 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import { faUser, faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+import {
+  faTwitter,
+  faInstagram,
+  faFacebook,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+
+library.add(
+  faUser,
+  faPhone,
+  faEnvelope,
+  faTwitter,
+  faInstagram,
+  faFacebook,
+  faYoutube
+);
