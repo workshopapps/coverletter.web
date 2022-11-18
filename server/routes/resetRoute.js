6f260e159dfc57be2372b55473ecaaf0eed1906e
Muhammad <muhammad17gb@gmail.com>
2022-11-18 20:17:31 +0100

    implement OTP validation and password reset

--- server/routes/resetRoute.js
+++ server/routes/resetRoute.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { forgotPassword, validateOtp } = require("../controllers/resetPassword");
+const { resetPassword, validateOtp } = require("../controllers/resetPassword");
 
 // forgot password route here
-router.post("/forgot", forgotPassword);
+router.post("/reset", resetPassword);
 router.post("/validate_otp", validateOtp);
 
 module.exports = router;
