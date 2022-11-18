4853dd97177b05495f63271c73e8204f5bf1a8d7
preshh0 <uniquep201@gmail.com>
2022-11-18 23:29:40 +0100

    Deleted a redundant file.

--- server/routes/contactRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { contact } = require("../controllers/contact");
-
-router.post("/", contact);
-
-module.exports = router;
