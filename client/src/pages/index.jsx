6f1fef6f3d2940c1c4414614b20ed085d60c208e
Cornerstone <fortunecornerstone@gmail.com>
2022-11-18 20:13:39 +0100

    feat: Added mobile scroll && banner images

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -5,5 +5,4 @@ import PrivacyPolicy from "./PrivacyPolicy";
 import ContactUs from "./ContactUs";
 import Faq from "./Faq";
 
-export { Home, History, ErrorPage, ContactUs, PrivacyPolicy, Faq   };
-
+export { Home, History, ErrorPage, ContactUs, PrivacyPolicy, Faq };
