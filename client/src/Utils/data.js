c937d7e7290c6bb0ce7663b9669f423bf7cc9302
antomuangigitau <muangigitau@gmail.com>
2022-11-19 15:52:24 +0300

    fix(FE-17): added changes to customer stories iteration

--- client/src/Utils/data.js
+++ client/src/Utils/data.js
@@ -78,11 +78,8 @@ export const items = [
 		read: "Read customer story",
 		arrow: require("../Assets/arrow-right.png"),
 	},
-];
-
-export const testimonials = [
 	{
-		id: 1,
+		id: 3,
 		img: require("../Assets/Rectangle 23.png"),
 		story: "Customer Story",
 		text: "How i used the cover letter generated to get the job",
@@ -90,7 +87,7 @@ export const testimonials = [
 		arrow: require("../Assets/arrow-right.png"),
 	},
 	{
-		id: 2,
+		id: 4,
 		img: require("../Assets/Rectangle 24.png"),
 		story: "Customer Story",
 		text: "How i used the cover letter generated to get the job",
@@ -98,7 +95,7 @@ export const testimonials = [
 		arrow: require("../Assets/arrow-right.png"),
 	},
 	{
-		id: 3,
+		id: 5,
 		img: require("../Assets/Rectangle 25.png"),
 		story: "Customer Story",
 		text: "How i used the cover letter generated to get the job",
@@ -106,7 +103,7 @@ export const testimonials = [
 		arrow: require("../Assets/arrow-right.png"),
 	},
 	{
-		id: 4,
+		id: 6,
 		img: require("../Assets/Rectangle 26.png"),
 		story: "Customer Story",
 		text: "How i used the cover letter generated to get the job",
@@ -114,7 +111,7 @@ export const testimonials = [
 		arrow: require("../Assets/arrow-right.png"),
 	},
 	{
-		id: 5,
+		id: 7,
 		img: require("../Assets/Rectangle 27.png"),
 		story: "Customer Story",
 		text: "How i used the cover letter generated to get the job",
@@ -122,7 +119,7 @@ export const testimonials = [
 		arrow: require("../Assets/arrow-right.png"),
 	},
 	{
-		id: 6,
+		id: 8,
 		img: require("../Assets/Rectangle 28.png"),
 		story: "Customer Story",
 		text: "How i used the cover letter generated to get the job",
@@ -130,6 +127,57 @@ export const testimonials = [
 		arrow: require("../Assets/arrow-right.png"),
 	},
 ];
+
+// export const testimonials = [
+// 	{
+// 		id: 1,
+// 		img: require("../Assets/Rectangle 23.png"),
+// 		story: "Customer Story",
+// 		text: "How i used the cover letter generated to get the job",
+// 		read: "Read customer story",
+// 		arrow: require("../Assets/arrow-right.png"),
+// 	},
+// 	{
+// 		id: 2,
+// 		img: require("../Assets/Rectangle 24.png"),
+// 		story: "Customer Story",
+// 		text: "How i used the cover letter generated to get the job",
+// 		read: "Read customer story",
+// 		arrow: require("../Assets/arrow-right.png"),
+// 	},
+// 	{
+// 		id: 3,
+// 		img: require("../Assets/Rectangle 25.png"),
+// 		story: "Customer Story",
+// 		text: "How i used the cover letter generated to get the job",
+// 		read: "Read customer story",
+// 		arrow: require("../Assets/arrow-right.png"),
+// 	},
+// 	{
+// 		id: 4,
+// 		img: require("../Assets/Rectangle 26.png"),
+// 		story: "Customer Story",
+// 		text: "How i used the cover letter generated to get the job",
+// 		read: "Read customer story",
+// 		arrow: require("../Assets/arrow-right.png"),
+// 	},
+// 	{
+// 		id: 5,
+// 		img: require("../Assets/Rectangle 27.png"),
+// 		story: "Customer Story",
+// 		text: "How i used the cover letter generated to get the job",
+// 		read: "Read customer story",
+// 		arrow: require("../Assets/arrow-right.png"),
+// 	},
+// 	{
+// 		id: 6,
+// 		img: require("../Assets/Rectangle 28.png"),
+// 		story: "Customer Story",
+// 		text: "How i used the cover letter generated to get the job",
+// 		read: "Read customer story",
+// 		arrow: require("../Assets/arrow-right.png"),
+// 	},
+// ];
 
 export const contents = [
 	{
@@ -142,33 +190,143 @@ export const contents = [
 		companies and individuals design their products, excellently . He believes the surroundings you 
 		work in should be a reflection of who you are. His goal is to provide the most creative, user 
 		friendly  and responsive designs to the company or individuals he works for.`,
-		challengeTalk: `Faruq Abdulsalam  was struggling to provide a seamless cover letter across the 
-		jobs he applied for online and physical. As a result he couldn't get his dream job and one day 
-		received a feedback from one of the companies he applied for on why he is not offered the job. 
-		He needed a way to connect his CV document to the cover letter attached to his documents while 
-		applying to get a job with this companies.`,
-		reject: `Due to the reject of the last  cover letter, I was feeling so bad and started doubting my 
-		capabilities until I was referred to Aplicar cover letter genertaor  by a friend.`,
-		img2: require("../Assets/Ellipse 12.png"),
-		solutionTalk: `The Aplicar  cover letter generator allows Faruq Abdulsalam to merge his CV with with 
-		the cover letter process. Thanks to Aplicar cover letter generator, the website and mobile app can 
-		create deeper and more personal cover letter interacting with company standard through automated 
-		recommendations. Now Aplicar use the CV to generate cover letter content.`,
-		outcome: `The Aplicar cover letter generator has helped Faruq Abdulsalam to generate standard cover 
-		letter. The goal of convincing the companies with a personalised service is now simpler to achieve 
-		than ever. As a result he has seen a good job immediately`,
+		textContents: [
+			{
+				id: 1,
+				title: "Challenge",
+				challengeTalk: `Faruq Abdulsalam  was struggling to provide a seamless cover letter across the 
+				jobs he applied for online and physical. As a result he couldn't get his dream job and one day 
+				received a feedback from one of the companies he applied for on why he is not offered the job. 
+				He needed a way to connect his CV document to the cover letter attached to his documents while 
+				applying to get a job with this companies.`,
+				reject: `Due to the reject of the last  cover letter, I was feeling so bad and started doubting my 
+				capabilities until I was referred to Aplicar cover letter genertaor  by a friend.`,
+				img2: require("../Assets/Ellipse 12.png"),
+				name: "Faruq Abdulsalam",
+				career: "Product Designer",
+			},
+			{
+				id: 2,
+				title: "Solution",
+				challengeTalk: `The Aplicar  cover letter generator allows Faruq Abdulsalam to merge his CV with with the cover letter process. 
+				Thanks to Aplicar cover letter generator, the website and mobile app can create deeper and more personal cover letter 
+				interacting with company standard through automated recommendations. Now Aplicar use the CV to generate cover letter content.`,
+				reject: `Aplicar cover letter made my cover letter easy by uploading my CV and interpreting the information from my CV 
+				into the cover letter. Got my cover letter in kess than 10 mins.`,
+				img2: require("../Assets/Ellipse 12.png"),
+				name: "Faruq Abdulsalam",
+				occupation: "Product Designer",
+			},
+			{
+				id: 3,
+				title: "Outcome",
+				challengeTalk: `The Aplicar cover letter generator has helped Faruq Abdulsalam to generate standard cover letter. The goal of 
+				convincing the companies with a personalised service is now simpler to achieve than ever. As a result he has seen a good job 
+				immediately`,
+				reject: `The outcome of this cover letter generated from Aplicar cover letter generator gave me an instant job in 1 week.`,
+				img2: require("../Assets/Ellipse 12.png"),
+				name: "Faruq Abdulsalam",
+				occupation: "Product Designer",
+			},
+		],
 	},
 	{
 		id: 2,
-		title: "How he used the cover letter generated to get a job",
-		img: require("../Assets/Rectangle22.png"),
+		title: "With no job experience, she landed a job interview.",
+		img: require("../Assets/Rectangle23.png"),
 		talk: "Mark Doyinsola",
 		position: "Financial Analyst",
+		introTalk: `Mark Doyinsola is a finance graduate without any prior work experience. She believes that everyone deserves a chance to 
+		show off their skills. It is her goal to support any company's finance team in whatever way she can and also to grow up to be a leader 
+		in finance.`,
+		textContents: [
+			{
+				id: 1,
+				title: "Challenge",
+				challengeTalk: `Doyinsola had no prior experience and has been finding it difficult to get a role as most jobs need someone 
+				with experience. Eventually, she got a job vacancy that required some skills and no experience, but Doyinsola is more of a 
+				technical person than a writer and has trouble writing a cover letter.`,
+				reject: `I did not have the experience writting a cover letter as it is my first job interview but i got introduced to Aplicar cover 
+				letter`,
+				img2: require("../Assets/Ellipse 13.png"),
+				name: "Mark Doyinsola",
+				occupation: "Financial Analyst",
+			},
+			{
+				id: 2,
+				title: "Solution",
+				challengeTalk: `By using the Aplicar cover letter, she can provide additional information about her skill set and projects 
+				she has worked on, rather than just using information from her CV. With Aplicar, she can create a cover letter that 
+				emphasizes her strength ( her skill set) rather than her weakness (her lack of experience). In order to generate the contents 
+				of the cover letter, Aplicar uses additional information.`,
+				reject: `I just used Aplicar to write my cover letter and I loved how it turned out. The interface is simple and well-designed. 
+				It was easy to use and I was impressed with the end result.`,
+				img2: require("../Assets/Ellipse 13.png"),
+				name: "Mark Doyinsola",
+				occupation: "Financial Analyst",
+			},
+			{
+				id: 3,
+				title: "Outcome",
+				challengeTalk: `The Aplicar cover letter generator helped Doyinsola Mark to generate a cover letter that landed her an 
+				interview. The hiring manager specifically commended her skill sets as highlighted in her cover letter. Although she didn't 
+				get the job, she now has a cover letter she can use to apply for other jobs and will be able to prove herself during the 
+				interview phase.`,
+				reject: `I had no idea what to say in the letter, but Aplicar made it so easy! And got a job interview.`,
+				img2: require("../Assets/Ellipse 13.png"),
+				name: "Mark Doyinsola",
+				occupation: "Financial Analyst",
+			},
+		],
+	},
+	{
+		id: 3,
+		title: "How he used the cover letter generated to get a job",
+		img: require("../Assets/Rectangle22.png"),
+		talk: "Faruq Abdulsalam",
+		position: "Product Designer",
 		introTalk: `Faruq Abdulsalam is a Product designer wwith 2 years experience and he has helped 
 		companies and individuals design their products, excellently . He believes the surroundings you 
 		work in should be a reflection of who you are. His goal is to provide the most creative, user 
-		friendly  and responsive designs to the company or individuals he works for,`,
-		challengeTalk: `Faruq Abdulsalam  was struggling to provide a seamless cover letter across the jobs he applied for online and physical. As a result he couldn't get his dream job and one day received a feedback from one of the companies he applied for on why he is not offered the job. He needed a way to connect his CV document to the cover letter attached to his documents while applying to get a job with this companies.`,
-		reject: "Due to the reject of the last  cover letter, I was feeling so bad and started doubting my capabilities until I was referred to Aplicar cover letter genertaor  by a friend.",
+		friendly  and responsive designs to the company or individuals he works for.`,
+		textContents: [
+			{
+				id: 1,
+				title: "Challenge",
+				challengeTalk: `Faruq Abdulsalam  was struggling to provide a seamless cover letter across the 
+				jobs he applied for online and physical. As a result he couldn't get his dream job and one day 
+				received a feedback from one of the companies he applied for on why he is not offered the job. 
+				He needed a way to connect his CV document to the cover letter attached to his documents while 
+				applying to get a job with this companies.`,
+				reject: `Due to the reject of the last  cover letter, I was feeling so bad and started doubting my 
+				capabilities until I was referred to Aplicar cover letter genertaor  by a friend.`,
+				img2: require("../Assets/Ellipse 12.png"),
+				name: "Faruq Abdulsalam",
+				career: "Product Designer",
+			},
+			{
+				id: 2,
+				title: "Solution",
+				challengeTalk: `The Aplicar  cover letter generator allows Faruq Abdulsalam to merge his CV with with the cover letter process. 
+				Thanks to Aplicar cover letter generator, the website and mobile app can create deeper and more personal cover letter 
+				interacting with company standard through automated recommendations. Now Aplicar use the CV to generate cover letter content.`,
+				reject: `Aplicar cover letter made my cover letter easy by uploading my CV and interpreting the information from my CV 
+				into the cover letter. Got my cover letter in kess than 10 mins.`,
+				img2: require("../Assets/Ellipse 12.png"),
+				name: "Faruq Abdulsalam",
+				occupation: "Product Designer",
+			},
+			{
+				id: 3,
+				title: "Outcome",
+				challengeTalk: `The Aplicar cover letter generator has helped Faruq Abdulsalam to generate standard cover letter. The goal of 
+				convincing the companies with a personalised service is now simpler to achieve than ever. As a result he has seen a good job 
+				immediately`,
+				reject: `The outcome of this cover letter generated from Aplicar cover letter generator gave me an instant job in 1 week.`,
+				img2: require("../Assets/Ellipse 12.png"),
+				name: "Faruq Abdulsalam",
+				occupation: "Product Designer",
+			},
+		],
 	},
 ];
