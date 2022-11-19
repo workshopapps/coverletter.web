6fe52b9b45c3c08ef3ced85acad777c81f438053
Samuel Seibidor <samrythmn@gmail.com>
2022-11-19 02:02:48 +0100

    feat: added more icons to the library

--- client/src/fontawesome.js
+++ client/src/fontawesome.js
@@ -1,20 +1,26 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 
-import { faUser, faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+	faUser,
+	faPhone,
+	faEnvelope,
+	faChevronDown,
+} from "@fortawesome/free-solid-svg-icons";
 
 import {
-  faTwitter,
-  faInstagram,
-  faFacebook,
-  faYoutube,
+	faTwitter,
+	faInstagram,
+	faFacebook,
+	faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
 
 library.add(
-  faUser,
-  faPhone,
-  faEnvelope,
-  faTwitter,
-  faInstagram,
-  faFacebook,
-  faYoutube
+	faUser,
+	faPhone,
+	faEnvelope,
+	faChevronDown,
+	faTwitter,
+	faInstagram,
+	faFacebook,
+	faYoutube
 );
