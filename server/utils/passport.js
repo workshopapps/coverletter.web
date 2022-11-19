fd8fd5d56c68a81447642fdb1890405026ea8b19
lakes <mikededeji@gmail.com>
2022-11-19 10:52:37 +0100

    feat: googlr-authentication

--- server/utils/passport.js
+++ server/utils/passport.js
@@ -1,5 +1,5 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy
-const User = require('../models/Google')
+const User = require('../models/User')
 module.exports = function (passport) {
  passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
