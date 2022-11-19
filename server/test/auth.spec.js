9ac66079630e469e8cf47333653f890533d5032e
Temitope Agbaje <agbajeinioluwa@gmail.com>
2022-11-19 15:13:46 +0100

    feat (BE-6): Added more test cases

--- server/test/auth.spec.js
+++ server/test/auth.spec.js
@@ -1,11 +1,10 @@
 const supertest = require("supertest");
 const mongoose = require("mongoose");
 const app = require("../app");
-const UserModel = require("../models/User");
 
 require("dotenv").config();
 
-describe("Auth: Signup", () => {
+describe("Auth", () => {
 	/* Connecting to the database before each test. */
 	beforeAll(async () => {
 		await mongoose.connect(process.env.MONGO_URI);
@@ -19,12 +18,60 @@ describe("Auth: Signup", () => {
 	it("should signup a user", async () => {
 		const response = await supertest(app).post("/api/v1/auth/signup").send({
 			name: "Jane Doe",
-			email: "janed@gmail.com",
-			password: "Jam123",
+			email: "jao@gmail.com",
+			password: "Janio123",
 		});
 
 		console.log(response.body);
 		expect(response.status).toBe(201);
-		expect(response.body).toHaveProperty("user");
+		expect(response.body).toHaveProperty("Signup was successful.");
+	});
+
+	it("should not signup a user", async () => {
+		const response = await supertest(app).post("/api/v1/auth/signup").send({
+			name: "Jane Doe",
+			email: "jao@gmail.com",
+			password: "Janio123",
+		});
+
+		console.log(response.body);
+		expect(response.status).toBe(400);
+		expect(response.body).toHaveProperty("Email already exists.");
+	});
+
+	it("should signup a user", async () => {
+		const response = await supertest(app)
+			.post("/api/v1/auth/login")
+			.set("content-type", "application/json")
+			.send({
+				email: "jao@gmail.com",
+				password: "Janio123",
+			});
+
+		console.log(response.body);
+		expect(response.status).toBe(201);
+		expect(response.body.status).toHaveProperty("success");
+		expect(response.body.token).toHaveProperty(token);
+	});
+	it("should verify a user", async () => {
+		const response = await supertest(app).post("/api/v1/auth/verify").send({
+			otp: "12345",
+		});
+
+		console.log(response.body);
+		expect(response.status).toBe(201);
+		expect(response.body).toHaveProperty(
+			"User has been successfully verified"
+		);
+	});
+
+	it("should verify a user", async () => {
+		const response = await supertest(app).post("/api/v1/auth/verify").send({
+			otp: "1234",
+		});
+
+		console.log(response.body);
+		expect(response.status).toBe(400);
+		expect(response.body).toHaveProperty("Verification failed");
 	});
 });
