98c488f7bf85c59d7349476e25b4c13b6176caca
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-11-19 16:01:06 +0100

    update

--- server/app.js
+++ server/app.js
@@ -24,8 +24,6 @@ app.use(
 const authRoutes = require("./routes/authRoutes");
 const templateRoutes = require("./routes/templateRoutes");
 const cvToCoverLetterRoutes = require("./routes/cvToCoverLetterRoutes");
-// // database
-// const connectDB = require("./db/connect");
 
 app.use(
 	"/cvg-documentation",
