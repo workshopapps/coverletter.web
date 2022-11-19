98fb6c3ba0130d6879e06c20d96605b915f595c2
oyinkansola <adebayoalameen88@gmail.com>
2022-11-19 11:02:25 +0100

    feat: Created the Upload page and the Input page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
 import "./App.css";
-import { Home, ErrorPage, ContactUs, Faq } from "./pages";
+import React,{useState} from "react";
+import {  ErrorPage, ContactUs, Faq } from "./pages";
 import { ScrollToTop } from "./Components";
 import { Header, Footer } from "./Layouts";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Upload from "./pages/upload/Upload";
+import UploadData from "./pages/uploadData/UploadData";
+export const UserContext = React.createContext()
+
 const App = () => {
+	const [file,setFile]= useState()
+	const [fileName,setFileName]= useState('')
+	const Provider = UserContext.Provider
 	return (
+		<>
+		<Provider value= {{ file, setFile, fileName, setFileName }}>
 		<Router>
 			<ScrollToTop>
 				{/* place nav bar here*/}
 				<Header />
 				<Routes>
-					<Route path="/" element={<Home />} />
+					<Route path="/" element={<Upload />} />
+					<Route path="/upload-data" element={<UploadData />} />
 					<Route path="/contactus" element={<ContactUs />} />
 					<Route path="/faq" element={<Faq />} />
 					<Route path="*" element={<ErrorPage />}></Route>
@@ -19,6 +30,8 @@ const App = () => {
 				{/* place footer here */}
 			</ScrollToTop>
 		</Router>
+		</Provider>
+		</>
 	);
 };
 
