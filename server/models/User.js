8b4d9f5140d6d983ccafc1b3c313a06e2fa358c7
Okeke Chigozie <81691598+bertinchigozie@users.noreply.github.com>
2022-11-19 10:55:23 +0100

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -39,7 +39,7 @@ UserSchema.pre("save", async function () {
 	this.password = await bcrypt.hash(this.password, salt);
 });
 
-UserSchema.methods.createJWT = function (_id) {
+UserSchema.methods.createJWT = function () {
 	return jwt.sign(
 		{
 			userId: this._id,
