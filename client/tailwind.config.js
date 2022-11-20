ced475c0eb43ac792fa82f89b1348598820dba5e
antomuangigitau <muangigitau@gmail.com>
2022-11-20 14:54:14 +0300

    fix(FE-17): removed the screen sizes on tailwind file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -53,26 +53,6 @@ module.exports = {
 			white: "#FFFFFF",
 			backgroundWhite: "#FFFFFF",
 		},
-
-		screens: {
-			xxs: "320px",
-			// => @media (min-width: 320px) { ... }
-
-			xs: "480px",
-			// => @media (min-width: 480px) { ... }
-
-			sm: "640px",
-			// => @media (min-width: 640px) { ... }
-
-			md: "768px",
-			// => @media (min-width: 768px) { ... }
-
-			lg: "1024px",
-			// => @media (min-width: 1024px) { ... }
-
-			xl: "1280px",
-			// => @media (min-width: 1280px) { ... }
-		},
 	},
 	plugins: [],
 };
