9cdb29453ffc398655be973bd8f32ccfe82b51e2
antomuangigitau <muangigitau@gmail.com>
2022-11-20 23:48:57 +0300

    fix(FE-17): added more styling on this page

--- client/src/pages/SingleProduct.jsx
+++ client/src/pages/SingleProduct.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 import { contents } from "../Utils/data";
-import quoteUp from "../Assets/quote-up.png";
-import quoteDown from "../Assets/quote-down.png";
 import arrowLeft from "../Assets/arrow-left.png";
 const SingleProduct = () => {
 	const { customerId } = useParams();
@@ -12,30 +10,38 @@ const SingleProduct = () => {
 	const { id, title, img, name, occupation, introTalk, textContents } = item;
 	return (
 		<main className="bg-background">
-			<section className="px-6 md:py-17 md:px-24" key={id}>
-				<div className="xxs:max-md: block md:grid grid-cols-2 gap-x-[25px] pt-[72px]">
+			<section className="relative px-6 md:py-17 md:px-24" key={id}>
+				<Link
+					className="absolute top-3 flex items-center gap-x-3 pt-[19px] pb-12"
+					to="/customerstories"
+				>
+					<img src={arrowLeft} alt="" />
+					Customer Stories
+				</Link>
+				<div className="grid md:grid-cols-2 gap-x-[25px] pt-[72px]">
 					<div>
-						<Link
-							className="flex items-center gap-x-3 mb-8"
-							to="/customerstories"
-						>
-							<img src={arrowLeft} alt="" />
-							Customer Stories
-						</Link>
-						<h3 className="text-[32px] xxs:max-md:leading-[40px] md:max-lg:text-4xl md:text-[56px] leading-leading-10">
+						<h3 className="text-[32px] mb-[22px] leading-10 md:text-[44px] md:leading-[48px] lg:text-[56px] lg:leading-[72px] lg:mb-8">
 							{title}
 						</h3>
-						<p className="text-grey400 mt-8 leading-leading-4 text-base font-normal">
+						<p className="text-grey400 text-base font-normal">
 							Talking with{" "}
 							<span className="text-[#000]">{name}</span> ,
 							{occupation}
 						</p>
 					</div>
-					<img className="xxs:max-md:order-first" src={img} alt="" />
+					<img
+						className="-order-1 mb-[19.21px] md:order-1"
+						src={img}
+						alt=""
+					/>
 				</div>
-				<div className="mt-[65px] mb-[72px]">
-					<h3 className="md:text-[40px]">Introduction</h3>
-					<p className="text-base leading-6">{introTalk}</p>
+				<div className="mt-[33px] md:mt-[65px] mb-[72px]">
+					<h3 className="text-2xl leading-9 md:text-[40px]">
+						Introduction
+					</h3>
+					<p className="text-grey400 pr-0 text-base leading-6 lg:pr-[282px]">
+						{introTalk}
+					</p>
 				</div>
 				<div>
 					{textContents.map((textContent) => {
@@ -50,33 +56,28 @@ const SingleProduct = () => {
 						} = textContent;
 						return (
 							<div
-								className="xxs:max-md:pr-0 md:pb-[72px] lg:pr-[487px]"
+								className="pr-0 pb-[72px] lg:pr-[487px]"
 								key={id}
 							>
-								<h3 className="md:text-[40px]">{title}</h3>
+								<h3 className="text-2xl leading-9 md:text-[40px]">
+									{title}
+								</h3>
 								<p className="text-grey400 font-normal lg:w-[857px]">
 									{challengeTalk}
 								</p>
-								<div className="relative pt-[72px] pb-6">
-									<img
-										className="absolute left-0"
-										src={quoteUp}
-										alt=""
-									/>
-									<p className="text-base text-grey400 leading-6 font-semibold">
-										{reject}
-									</p>
-									<img
-										className="absolute right-8"
-										src={quoteDown}
-										alt=""
-									/>
-								</div>
+								<p className=" pt-[33px] pb-6 text-base text-grey400 leading-6 font-semibold md:pt-[72px] md:pb-6">
+									{reject}
+								</p>
+
 								<div className="flex">
 									<img src={img2} alt="" />
-									<div className="ml-[14px]">
-										<p>{name}</p>
-										<p>{occupation}</p>
+									<div className="text-grey400 ml-[12px]">
+										<p className="text-base font-bold">
+											{name}
+										</p>
+										<p className="text-base font-normal">
+											{occupation}
+										</p>
 									</div>
 								</div>
 							</div>
