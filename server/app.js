d9c55c24d92d1ace9624974a31a4d909eecf26c2
ikwuh jeffery enokela <ikwuhj@gmail.com>
2022-11-20 12:38:54 +0100

    fix:fixed pages import

--- server/app.js
+++ server/app.js
@@ -62,9 +62,9 @@ app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 const start = async () => {
 	try {
-		// connectDB(process.env.MONGO_URI).then(() => {
-		// 	console.log("Connection succesful");
-		// });
+		connectDB(process.env.MONGO_URI).then(() => {
+			console.log("Connection succesful");
+		});
 		app.listen(port, () =>
 			console.log(`Server is listening on port ${port}...`)
 		);
