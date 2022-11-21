0e0687e2724509276b65c1894a371b540b514dd7
Muhammad <muhammad17gb@gmail.com>
2022-11-21 11:00:42 +0100

    delete old reset password files

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,12 +5,16 @@ const {
 	updatePassword,
 	forgotPassword,
 	verify,
-	login
+	login,
+	resetPassword,
+	validateOTP,
 } = require("../controllers/authController");
 
 //Add your routes here
 router.post("/signup", register);
 router.post("/verify", verify);
+router.post("/reset_password", resetPassword);
+router.post("/validate_otp", validateOTP);
 router.put("/updatePassword", updatePassword);
 router.post("/login", login);
 router.post("/forgotPassword", forgotPassword);
