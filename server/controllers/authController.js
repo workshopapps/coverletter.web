adec2e48f4a2200c7f8026ab438263ba75a090ac
Muhammad <muhammad17gb@gmail.com>
2022-11-21 10:57:48 +0100

    move change password auth functions to auth controller files

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,7 +5,6 @@ const { BadRequestError } = require("../errors");
 const sendEmail = require("../utils/sendEmail");
 const crypto = require("crypto");
 
-
 const register = async (req, res) => {
 	const { email, name, password } = req.body;
 
@@ -145,11 +144,57 @@ const forgotPassword = async (req, res, next) => {
 		return next(new BadRequestError("Error Sending Email"));
 	}
 };
+const resetPassword = async (req, res) => {
+	const { password, email, confirmPassword } = req.body;
 
+	if (password !== confirmPassword) {
+		throw new BadRequestError("passwords must be similar");
+	}
+
+	const user = await User.findOne({ email: email });
+
+	if (!user) {
+		throw new BadRequestError("email not found in database");
+	}
+
+	const comparePassword = await user.compare(hashedPassword, user.password);
+	if (!comparePassword) {
+		throw new BadRequestError("Passwords fields can only be unique");
+	}
+
+	user.password = password;
+	user.passwordResetToken = undefined;
+	user.passwordResetExpires = undefined;
+	await user.save();
+
+	res.status(StatusCodes.CREATED).json({
+		msg: "Password change was successful.",
+	});
+};
+
+const validateOTP = async (req, res) => {
+	const { otp, email } = req.body;
+	//console.log(otp);
+	const user = await User.findOne({ email });
+	if (!user) {
+		throw new BadRequestError("email not found in database");
+	}
+
+	const verifyOTP = await user.compare(otp, user.otp);
+	if (!verifyOTP) {
+		throw new BadRequestError("OTP is invalid or expired");
+	}
+
+	res.status(StatusCodes.CREATED).json({
+		msg: "otp verification was successful.",
+	});
+};
 module.exports = {
 	register,
 	login,
 	forgotPassword,
 	updatePassword,
 	verify,
+	resetPassword,
+	validateOTP,
 };
