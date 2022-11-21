dfce5c433e7b069282453db793bcbb491388085e
samyCode3 <107084481+samyCode3@users.noreply.github.com>
2022-11-21 01:37:55 +0100

    Update contactController.js

--- server/controllers/contactController.js
+++ server/controllers/contactController.js
@@ -5,26 +5,7 @@ const { BadRequestError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 
 const contact = async (req, res) => {
-	const { email, fullname, phone, subject, description } = req.body;
-
-	const contactError = ContactValidation.contactUs(
-		fullname,
-		email,
-		subject,
-		phone,
-		description
-	);
-
-	if (contactError) {
-		throw new BadRequestError(contactError);
-	} else 
-    {
-       await sendEmail(email, subject, description).then((result) => {
-			if (result) {
-				res.status(StatusCodes.CREATED).json(result);
-			}
-		});
-    }
+  res.send("hello")
 
 };
 module.exports = { contact };
