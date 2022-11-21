8c6378a0a7947b8d252cf9f974fa989b937570e5
samyCode3 <107084481+samyCode3@users.noreply.github.com>
2022-11-21 01:42:41 +0100

    Update contactRoutes.js

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -1,8 +1,18 @@
-const express = require("express");
-const router = express.Router();
-const ContactController  = require("../controllers/contactController");
+// const express = require("express");
+// const router = express.Router();
+// const ContactController  = require("../controllers/contactController");
 
 
-router.post("/contact", ContactController.contact);
+// router.post("/contact", ContactController.contact);
 
 module.exports = router;
+const express = require("express");
+const router = express.Router();
+const { cvUpload } = require("../controllers/cvUploadController");
+// const { sendCoverLetter } = require("../controllers/coverLetterGenerator");
+
+router.post("/uploads", cvUpload);
+// router.post("/generate", sendCoverLetter);
+
+module.exports = router;
+
