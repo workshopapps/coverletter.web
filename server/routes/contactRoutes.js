ae3657b2367d1cb34e24f403a0fcdcacd2a88ddf
samson onifade <107084481+samyCode3@users.noreply.github.com>
2022-11-24 09:23:50 +0100

    update

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const ContactController  = require("../controllers/contactController");
+const ContactController  = require("../controllers/contactUsController");
 
 
 router.post("/contact", ContactController.contact);
