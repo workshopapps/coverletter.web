151b7d25da6d4f92136da00b2a5dd8fa6e55f384
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-11-24 19:18:09 +0100

    Chore(BE-16): Removed unused code

--- server/utils/sendPdfToEmail.js
+++ server/utils/sendPdfToEmail.js
@@ -1,8 +1,5 @@
 const nodemailer = require("nodemailer");
 
-const htmlMessage = `<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">`;
-
 module.exports = async (email, subject, fileName) => {
 	const fileFormat = fileName.split(".")[1].toLowerCase();
 	try {
