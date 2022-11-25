0402d3a0e3758448aec4d53083ae55f1da0401a4
jayGathu <jjgathu17@gmail.com>
2022-11-25 04:49:28 +0300

    feat: Google auth

--- server/utils/passport.js
+++ server/utils/passport.js
@@ -9,7 +9,7 @@ module.exports = function (passport) {
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: '/api/v1/auth/google/callback',
       },
-      async (accessToken, refreshToken, profile,email, done) => {
+      async (accessToken, refreshToken, profile, done) => {
         const newUser = {
           googleId: profile.id,
           name: profile.displayName,
