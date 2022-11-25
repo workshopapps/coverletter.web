7d09092497cef3d46c087760425c8b8611cdbb32
Delight-Fela-Steve <dfelastevegit@gmail.com>
2022-11-25 04:48:45 +0100

    fix/BE added try catch block to download routes

--- server/controllers/downloadController.js
+++ server/controllers/downloadController.js
@@ -2,25 +2,35 @@ const {convertToDoc} = require("../utils/convertToDoc");
 const fs = require("fs");
 
 const downloadCoverLetter = async function(req,res){
-    const data = req.body.data;
-    const content = `${data.company_name}\n${data.company_address}\n
-    ${data.city},${data.country}\n` + data.cover_letter
+    try {
+        const data = req.body.data;
+        const content = `${data.company_name}\n${data.company_address}\n
+        ${data.city},${data.country}\n` + data.cover_letter
+    
+        const pathToFile = convertToDoc(content,data.format);
+        let full_route = req.get('host') + req.originalUrl
+        let url = `http://${full_route}?file=${pathToFile}`
+        return res.status(201).json({status:"success",redirect:url});
+    } catch (error) {
+        return res.status(500).json({"message":"Oops! The problem is from us"})
+    }
 
-    const pathToFile = convertToDoc(content,data.format);
-    let full_route = req.get('host') + req.originalUrl
-    let url = `http://${full_route}?file=${pathToFile}`
-    return res.status(201).json({status:"success",redirect:url});
 }
 
 const download = async function(req,res){
-    const pathToFile = req.query.file
-    return res.download(pathToFile,(err)=>{
-        if (err){
-            return res.status(500).json({status:"failure"})
-        }else{
-            fs.unlink(pathToFile,(err)=>{if(err){console.log(err)}});
-        }
-    });
+    try {
+        const pathToFile = req.query.file
+        return res.download(pathToFile,(err)=>{
+            if (err){
+                return res.status(500).json({status:"failure"})
+            }else{
+                fs.unlink(pathToFile,(err)=>{if(err){console.log(err)}});
+            }
+        });
+    } catch (error) {
+        return res.status(500).json({"message":"Oops! The problem is from us"})
+    }
+
     
 }
 
