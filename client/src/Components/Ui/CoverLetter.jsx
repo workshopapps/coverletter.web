7b993cb74d2aaefd6998de14d7829b47ad1d4f47
Itz-thevine <divine2christ@yahoo.com>
2022-11-25 17:52:42 +0100

    fix: Made adjustments to the preview and download page

--- client/src/Components/Ui/CoverLetter.jsx
+++ client/src/Components/Ui/CoverLetter.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-// import { useGlobalContext } from "../../context/context";
+import { useGlobalContext } from "../../context/context";
 const CoverLetter = () => {
-	// const { coverLetter: data } = useGlobalContext();
+	const { coverLetter: data } = useGlobalContext();
 
-	const data = {
-		cover_letter:
-			"\n\nDear Mr. / Ms. (Lastname): (or “Dear Campus Recruitment Representative/Hiring Manager”) \n \nRe:  Backend Developer at Google \n \nFirst paragraph: Briefly sum up your value to the employer by stating your interest in the \nposition and the organization. You should also list your relevant education in the opening. Your \ngoal is to capture the reader's attention and encourage them to read on! This paragraph \nhighlights what you will expand on and demonstrate in your resume. It summarizes your relevant \nand then indicates the top two or three skills that make you a great fit for the position. Also try \ndemonstrating your knowledge/interest in the organization by integrating something specific \nabout what you know about them (i.e., mission/mandate, projects/activities, research interests, \nstrategic goals) in relation to how you can contribute or skills you offer.  \n \nMiddle paragraphs: Further highlight your suitability for the position by expanding on your \nskills through specific examples of your previous accomplishments (i.e., work experience and \nroles, volunteer experience, and/or academic examples). Think from the employer’s point of view \nas you write. In what ways are you a good fit for the organization? Be sure to emphasize \noutcomes and results whenever possible. Try to keep your paragraphs to 3-5 concise, confident \nstatements. Remember that this letter will serve as a writing sample so grammar/spelling is \ncritical.  \n \nUse the closing paragraph to express your appreciation for reviewing your cover letter and \nresume.  This is also a great place to let them know how they can easily contact you. Be sure to \nlist that contacting you through the TRU Co-op program is also an option (email/phone).   \n \nSincerely, \n \nYour Signature (John Henry) \n \nTyped Name",
-		company_name: "Google",
-		company_address: "USA",
-		city: "California",
-		country: "USA",
-		years_of_exp: "3",
-		date: "12-11-2022",
-		recipient_name: "Mark",
-		recipient_department: "HR",
-		recipient_email: "t@email.com",
-		recipient_phone_no: "2223334446",
-		format: "pdf",
-	};
+	// const data = {
+	// 	cover_letter:
+	// 		"\n\nDear Mr. / Ms. (Lastname): (or “Dear Campus Recruitment Representative/Hiring Manager”) \n \nRe:  Backend Developer at Google \n \nFirst paragraph: Briefly sum up your value to the employer by stating your interest in the \nposition and the organization. You should also list your relevant education in the opening. Your \ngoal is to capture the reader's attention and encourage them to read on! This paragraph \nhighlights what you will expand on and demonstrate in your resume. It summarizes your relevant \nand then indicates the top two or three skills that make you a great fit for the position. Also try \ndemonstrating your knowledge/interest in the organization by integrating something specific \nabout what you know about them (i.e., mission/mandate, projects/activities, research interests, \nstrategic goals) in relation to how you can contribute or skills you offer.  \n \nMiddle paragraphs: Further highlight your suitability for the position by expanding on your \nskills through specific examples of your previous accomplishments (i.e., work experience and \nroles, volunteer experience, and/or academic examples). Think from the employer’s point of view \nas you write. In what ways are you a good fit for the organization? Be sure to emphasize \noutcomes and results whenever possible. Try to keep your paragraphs to 3-5 concise, confident \nstatements. Remember that this letter will serve as a writing sample so grammar/spelling is \ncritical.  \n \nUse the closing paragraph to express your appreciation for reviewing your cover letter and \nresume.  This is also a great place to let them know how they can easily contact you. Be sure to \nlist that contacting you through the TRU Co-op program is also an option (email/phone).   \n \nSincerely, \n \nYour Signature (John Henry) \n \nTyped Name",
+	// 	company_name: "Google",
+	// 	company_address: "USA",
+	// 	city: "California",
+	// 	country: "USA",
+	// 	years_of_exp: "3",
+	// 	date: "12-11-2022",
+	// 	recipient_name: "Mark",
+	// 	recipient_department: "HR",
+	// 	recipient_email: "t@email.com",
+	// 	recipient_phone_no: "2223334446",
+	// 	format: "pdf",
+	// };
 
 	return (
 		<div className="w-[98%] bg-textWhite border-grey300 border-2 py-8 px-7 rounded-lg">
