0f48803224a5a31cc41ee22a9b38e7fc37431cd1
samyCode3 <107084481+samyCode3@users.noreply.github.com>
2022-11-25 21:07:46 +0100

    update on the contact us validation

--- server/controllers/contactController.js
+++ server/controllers/contactController.js
@@ -1,4 +1,3 @@
-
 const ContactUsValidation = require("../utils/contactUs.validation");
 const sendEmail = require("../utils/sendEmail");
 const { BadRequestError } = require("../errors");
@@ -6,7 +5,7 @@ const { StatusCodes } = require("http-status-codes");
 
 const contact = async (req, res) => {
 	const { userEmail, fullName, phone, subject, description } = req.body;
-    const email = "aplicarorg@gmail.com"
+	const email = "aplicarorg@gmail.com";
 	const contactError = ContactUsValidation.ContactValidation(
 		fullName,
 		userEmail,
@@ -16,16 +15,11 @@ const contact = async (req, res) => {
 	);
 
 	if (contactError) {
-		throw new BadRequestError(contactError)
+		throw new BadRequestError(contactError);
 		return;
-	} else 
-    {
-      return await sendEmail(email, subject, description).then((result) => {
-			if (result) {
-			 return	res.status(StatusCodes.CREATED).json(result);
-			}
-		});
-    }
-
+	}
+	return await sendEmail(email, subject, description).then((result) => {
+		if (result) return res.status(StatusCodes.CREATED).json(result);
+	});
 };
 module.exports = { contact };
--- server/utils/contactUs.validation.js
+++ server/utils/contactUs.validation.js
@@ -1,24 +1,25 @@
 const ContactValidation = (
-   
-    fullName,
-    userEmail,
-    phone,
-    subject,
-    description
+	fullName,
+	userEmail,
+	phone,
+	subject,
+	description
 ) => {
-    const emailRegex = "/^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/";
-    const phoneRegex = "/^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/";
+	const emailRegex = "/^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/";
+	const phoneRegex =
+		"/^+?d{1,4}?[-.s]?(?d{1,3}?)?[-.s]?d{1,4}[-.s]?d{1,4}[-.s]?d{1,9}$/";
 
-    let result;
-    if (!userEmail || !fullName || !phone || !subject || !description) {
-            result = "All input are required";
-    } 
-    if (!phoneRegex.test(phone.toString())) {
-            result = "Invaild Phone Number";
-    } else 
-     if (!emailRegex.test(userEmail)) {
-            result = "Invaild Email entered";
-    }
-    return result;
+	let result;
+	if (!userEmail || !fullName || !phone || !subject || !description) {
+		result = "All input are required";
+	}
+	if (!phoneRegex.test(phone.toString())) {
+		result = "Invaild Phone Number";
+	}
+
+	if (!emailRegex.test(userEmail)) {
+		result = "Invaild Email entered";
+	}
+	return result;
 };
 module.exports = { ContactValidation };
