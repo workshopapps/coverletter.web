2c3dffd187312289bc959c9a54d4fa1ad6ce52c8
Samuel Seibidor <samrythmn@gmail.com>
2022-11-29 14:45:34 +0100

    fix: added disabled state to button

--- client/src/Components/Ui/Button.jsx
+++ client/src/Components/Ui/Button.jsx
@@ -1,9 +1,10 @@
 const Button = (props) => {
 	return (
 		<button
-			className={`${props.className}`}
+			className={`${props.className} disabled:bg-disabledDisabled`}
 			type={props.type || "button"}
 			onClick={props.onClick}
+			disabled={props.disabled}
 		>
 			<div className="flex items-center justify-center space-x-2">
 				<p>{props.children}</p>
