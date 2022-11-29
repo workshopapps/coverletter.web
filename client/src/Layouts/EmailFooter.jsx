119b5a90d4578344d714ca62eea2d59ed08bc052
danielswift10 <ojeyomi.daniel@gmail.com>
2022-11-29 22:51:01 +0100

    feat(FE-36): recreated and adjusted the header component for webpage

--- client/src/Layouts/EmailFooter.jsx
+++ client/src/Layouts/EmailFooter.jsx
@@ -2,7 +2,7 @@ const EmailFooter = () => {
 	return (
 		<footer>
 			<ul className="block text-center mt-[100px]">
-				<li className="inline-block mr-4 text-gray-700">©Aplicar</li>
+				<li className="inline-block mr-4 text-gray-700">©Coverly</li>
 				<li className="inline-block mr-4 text-gray-700">Contact</li>
 				<li className="inline-block mr-4 text-gray-700">
 					Privacy & Terms
