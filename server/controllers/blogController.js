07d66318391178a1362039c3357020e050e95a34
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-11-30 21:05:40 +0100

    Chore(BE-8): Progress made on blogcontroller

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,21 +1,27 @@
 require("dotenv").config();
 const Blog = require("../models/Blog");
+const mongoose = require("mongoose");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors");
 
 const createPost = async (req, res) => {
-	const { adminId, title, content, likes } = req.body;
-	const admin = await User.findOne(adminId);
-	if (!admin)
+	const { adminId, title, content } = req.body;
+	if (!mongoose.Types.ObjectId.isValid(adminId) || !admin) {
+		throw new BadRequestError(
+			"This adminId is not valid or the admin does not exsit in our database."
+		);
+	}
+	if (!adminId || !title || !content)
 		return res
-			.status(StatusCodes.NOT_FOUND)
-			.json({ message: "This admin does not exist in our database" });
-	const post = new Blog({ adminId, title, content, likes });
+			.status(StatusCodes.NO_CONTENT)
+			.json({ message: "All Fields are required" });
+	const admin = await User.findOne(adminId);
+	const post = new Blog({ adminId, title, content });
 	await post.save();
 
 	return res
 		.status(StatusCodes.CREATED)
-		.json("Creation of blog post was successful.");
+		.json({ message: "Creation of blog post was successful." });
 };
 
 module.exports = {
