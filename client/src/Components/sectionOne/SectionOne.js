dfa3ad1818b008d96d22a8fd76f343a194592ae9
Williams Ajayi Balogun <williamsdev.official@gmail.com>
2022-11-30 11:35:18 +0100

    feat(FE-29): Repair alignment for customer stories page

--- client/src/Components/sectionOne/SectionOne.js
+++ client/src/Components/sectionOne/SectionOne.js
@@ -1,28 +1,23 @@
-import React from "react";
-import pic from "./assets/pic.svg";
+import React,{useContext} from 'react'
+import pic from './assets/pic.svg';
+
 
 function SectionOne() {
-	return (
-		<div>
-			<main className="flex h-[418px] md:h-[50vh] lg:h-[70vh] lw:h-[75vh] bg-primaryDeep lg:px-3 lg:py-5 relative box-border overflow-hidden ">
-				<div className="text bottom-[15%] px-0  w-[83%] m-auto lg:bottom-[20%] flex flex-col px-[2rem] h-[30vh] lg:h-[60vh]  justify-center auto lw:w-[1250px] lw:m-auto lw:px-0">
-					<h1 className="font-bold  text-textWhite lg:text-[56px] md:text-[50px] text-4xl  lg:leading-[4rem] leading-[3rem] text-left lg:pr-[200px] lw:w-[700px] lw:px-0">
-						Create a Job-Landing Cover Letter in seconds
-					</h1>
-					<p className="text-grey100 text-left w-[100%] text-[20px] mt-3 lg:text-[40px] ">
-						100% Automated and Free
-					</p>
-				</div>
-				<div className="shape hidden md:flex">
-					<img
-						className=" w-[1440px] h-[440px] left-[300px] lw:bottom-[-50px] lw:left-[600px] lw:w-[1006px] bottom-[-150px] absolute"
-						src={pic}
-						alt="hero"
-					/>
-				</div>
-			</main>
-		</div>
-	);
+
+  return (
+    <div>
+         <main className="flex items-center h-[368px] md:h-[100vh] bg-primaryDeep md:px-3 md:py-5 relative box-border overflow-hidden ">
+        <div className="text relative md:bottom-[20%] flex flex-col md:px-[56px] md:-left-[5%] lg:left-0 lg:w-[80%]  lg:px-[50px] px-[2rem] h-[50vh] md:h-[auto]  md:w-[80%]  justify-center items-center auto z-10">
+          <h1 className='font-bold w-[100%] text-textWhite md:text-[45px] lg:text-[52px] text-4xl  md:leading-[4rem] text-left'>Create a Job-Landing Cover Letter in seconds</h1>
+          <p className='text-grey100 text-left w-[100%] text-[20px] mt-3 md:text-[25px] lg:text-[32px] '>100% Automated and Free</p>
+        </div>
+        <div className="shape hidden md:flex flex-auto">
+            <img className=' w-[1440px] h-[440px] left-[200px] lg:left-[300px] bottom-[-100px] md:bottom-[-110px] lg:bottom-[-100px] absolute'src={pic} alt="" />
+        </div>
+      </main>
+
+    </div>
+  )
 }
 
-export default SectionOne;
+export default SectionOne
