2d59fa3449782b104fd64eb4bdb18ce36e90ffb4
Idris Samir <samzidris11@gmail.com>
2022-12-01 22:13:37 +0100

    remove scroll to top button from page navbar and improve scroll to top button

--- client/src/pages/PrivacyPolicy.jsx
+++ client/src/pages/PrivacyPolicy.jsx
@@ -1,33 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Button from "../Components/Ui/Button";
-import { ReactComponent as ArrowUp } from "../Assets/arrow-up.svg";
-import { ReactComponent as ArrowBlue } from "../Assets/arrow-up-blue.svg";
 import BannerLogo from "../Assets/bannerLogo.png";
 import privacyBannerElements from "../Constants/privacyBannerElements";
 
 const PrivacyPolicy = () => {
-	const [displayArrow, setDisplayArrow] = useState(false);
-
-	const scrollUp = () => {
-		window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-	};
-
-	const listenToScroll = () => {
-		if (
-			document.body.scrollTop > 50 ||
-			document.documentElement.scrollTop > 50
-		) {
-			setDisplayArrow(true);
-		} else {
-			setDisplayArrow(false);
-		}
-	};
-
-	useEffect(() => {
-		window.addEventListener("scroll", listenToScroll);
-		return () => window.removeEventListener("scroll", listenToScroll);
-	}, []);
-
 	return (
 		<div className="bg-background">
 			<div
