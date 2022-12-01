cab8a7c6c9fe3a34843f11a8759284503260c2fb
Samuel Seibidor <samrythmn@gmail.com>
2022-12-01 18:49:58 +0100

    fix: Correct url and styling issues for socials

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Input, { TextField, Select } from "../Components/Ui/Form";
-import axios from "axios"
+import axios from "axios";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -9,50 +9,37 @@ const followUsData = [
 	{
 		icon: ["fab", "twitter"],
 		text: "Twitter",
-		link: "https://twitter.com/Coverly_org",
+		url: "https://twitter.com/coverlyng",
 	},
 	{
 		icon: ["fab", "instagram"],
 		text: "Instagram",
-		link: "http://www.instagram.com/Coverly_ng",
+		url: "http://www.instagram.com/coverly_ng",
 	},
 	{
 		icon: ["fab", "facebook"],
 		text: "Facebook",
-		link: "https://www.facebook.com/Coverly_org",
-	},
-	{
-		icon: ["fab", "youtube"],
-		text: "Youtube",
-		link: "https://www.youtube.com/Coverly_org",
+		url: "https://facebook.com/profile.php?id=100088156789141",
 	},
 ];
 
 const IconButton = (props) => {
 	const { text, icon, href } = props;
-	const content = (
+	return (
 		<div className="flex items-center flex-col">
-			<div className="h-10 w-10 flex items-center justify-center border-[1px] border-[#DCDCDC] rounded-full mb-1 hover:bg-[#e9e7e7]">
-				{icon && (
-					<FontAwesomeIcon icon={icon} className=" text-grey400" />
-				)}
-			</div>
-			<p className="text-sm text-grey400">{text}</p>
-		</div>
-	);
-	const holderClassName = "cursor-pointer flex space-y-2 items-center";
-	if (href)
-		return (
 			<a
-				href={href}
+				href={href || "#"}
 				target="_blank"
 				rel="noreferrer"
-				className={holderClassName}
+				className="h-10 w-10 flex items-center justify-center border-[1px] border-[#DCDCDC] rounded-full mb-1 hover:border-[#101010] text-xl text-[#101010] active:text-white active:bg-[#101010] active:border-[#DCDCDC]"
 			>
-				{content}
+				{icon && (
+					<FontAwesomeIcon icon={icon} />
+				)}
 			</a>
-		);
-	return <div className={holderClassName}>{content}</div>;
+			<p className="text-xs text-grey400">{text}</p>
+		</div>
+	);
 };
 
 const TertiaryButton = (props) => {
@@ -111,8 +98,8 @@ const FollowUsLinks = (props) => {
 		<div className={className}>
 			<p className="text-base text-grey400 font-bold mb-2">Follow Us</p>
 			<div className="flex space-x-5">
-				{followUsData.map(({ icon, text, link }, index) => (
-					<IconButton key={index} {...{ icon, text, href: link }} />
+				{followUsData.map(({ icon, text, url }, index) => (
+					<IconButton key={index} {...{ icon, text, href: url }} />
 				))}
 			</div>
 		</div>
@@ -120,35 +107,53 @@ const FollowUsLinks = (props) => {
 };
 
 const Alert = (props) => {
-	const {msg, type} = props
+	const { msg, type } = props;
 	const alertContainer = React.useRef(null);
 	const [height, setHeight] = React.useState("0px");
 
 	React.useEffect(() => {
-	  setHeight(!!msg ? "100px": "0px");
-	}, [msg, setHeight])
-	
-	return  <div ref={alertContainer} style={{maxHeight:height}} className="transition-all duration-200 overflow-hidden">
-		<div className={`w-full py-2 px-4 bg-${type||"error"}Main rounded flex justify-between items-center text-textWhite`}> 
-	<p className="text-sm">{msg}</p> <FontAwesomeIcon icon={["fas", "circle-info"]} />
-	</div>
-	</div>
-}
+		setHeight(!!msg ? "100px" : "0px");
+	}, [msg, setHeight]);
+
+	return (
+		<div
+			ref={alertContainer}
+			style={{ maxHeight: height }}
+			className="transition-all duration-200 overflow-hidden"
+		>
+			<div
+				className={`w-full py-2 px-4 bg-${
+					type || "error"
+				}Main rounded flex justify-between items-center text-textWhite`}
+			>
+				<p className="text-sm">{msg}</p>{" "}
+				<FontAwesomeIcon icon={["fas", "circle-info"]} />
+			</div>
+		</div>
+	);
+};
 
 const SuccessModal = (props) => {
-	const {open, onClose} = props;
-	if(!open) return <React.Fragment />
- 	return <div className="h-screen w-screen fixed top-0 left-0 z-50 bg-[#03296F7A] flex items-center justify-center" onClick={onClose}>
-		<div className="w-[250px] p-8 bg-[#fff] rounded flex items-center flex-col text-center space-y-2">
-			<div className=" bg-successMain rounded-full text-textWhite text-2xl h-[70px] w-[70px]  flex items-center justify-center border-8 border-successLight">
-				<FontAwesomeIcon icon={["fas", "check"]} />
+	const { open, onClose } = props;
+	if (!open) return <React.Fragment />;
+	return (
+		<div
+			className="h-screen w-screen fixed top-0 left-0 z-50 bg-[#03296F7A] flex items-center justify-center"
+			onClick={onClose}
+		>
+			<div className="w-[250px] p-8 bg-[#fff] rounded flex items-center flex-col text-center space-y-2">
+				<div className=" bg-successMain rounded-full text-textWhite text-2xl h-[70px] w-[70px]  flex items-center justify-center border-8 border-successLight">
+					<FontAwesomeIcon icon={["fas", "check"]} />
+				</div>
+				<h4 className="text-grey600 text-lg">Success</h4>
+				<p className="text-grey400 text-sm">
+					Thanks for contacting us, we will get back to you{" "}
+				</p>
+				<Button onClick={onClose}>Continue</Button>
 			</div>
-			<h4 className="text-grey600 text-lg">Success</h4>
-			<p className="text-grey400 text-sm">Thanks for contacting us, we will get back to you </p>
-			<Button onClick={onClose}>Continue</Button>
 		</div>
-	</div>
-}
+	);
+};
 
 const ContactUs = () => {
 	const emptyForm = {
@@ -162,20 +167,19 @@ const ContactUs = () => {
 	const [formData, setFormData] = React.useState(emptyForm);
 	const [errors, setErrors] = React.useState(emptyForm);
 	const [loading, setLoading] = React.useState(false);
-	const [alert, setAlert] = React.useState({msg:"", type:""});
+	const [alert, setAlert] = React.useState({ msg: "", type: "" });
 	const [openModal, setOpenModal] = React.useState(false);
 
 	const navigate = useNavigate();
 
 	React.useEffect(() => {
-	  return () => {
-		setOpenModal(false)
-	  }
-	}, [setOpenModal])
-	
+		return () => {
+			setOpenModal(false);
+		};
+	}, [setOpenModal]);
 
 	const handleChange = (id, value) => {
-		setAlert({msg:"", alert:""})
+		setAlert({ msg: "", alert: "" });
 		setFormData((prevFormData) => {
 			return { ...prevFormData, [id]: value };
 		});
@@ -220,19 +224,29 @@ const ContactUs = () => {
 
 	const submit = async (e) => {
 		e.preventDefault();
-		setLoading(true)
-		setAlert({msg:"", alert:""})
+		setLoading(true);
+		setAlert({ msg: "", alert: "" });
 		const errorResult = validate();
 		setErrors(validate());
 		if (!anyError(errorResult)) {
 			try {
-				const body = {fullName:formData.name, userEmail: formData.email, subject: formData.issue, description: formData.message, phone:formData.phone};
-				await axios.post("http://api.Coverly.hng.tech/api/v1/contact", body);
+				const backendApIurl = "http://api.coverly.hng.tech";
+				const body = {
+					fullName: formData.name,
+					userEmail: formData.email,
+					subject: formData.issue,
+					description: formData.message,
+					phone: formData.phone,
+				};
+				await axios.post(`${backendApIurl}/api/v1/contact`, body);
 				setFormData(emptyForm);
 				setLoading(false);
 				setOpenModal(true);
 			} catch (error) {
-				setAlert({msg:"Internal server error, please try again", type:"error"});
+				setAlert({
+					msg: "Internal server error, please try again",
+					type: "error",
+				});
 				console.error(error);
 			}
 		}
@@ -259,12 +273,12 @@ const ContactUs = () => {
 					<div className="flex flex-col lg:flex-row items-start lg:space-x-5 space-y-5 lg:space-y-0 my-10">
 						<TertiaryButton
 							label="Email Us"
-							text="Coverlyorg@gmail.com"
-							href="mailto:Coverlyorg@gmail.com"
+							text="coverlyorg@gmail.com"
+							href="mailto:coverlyorg@gmail.com"
 						/>
 						<TertiaryButton
 							label="Call Us"
-							text="+2349074265463"
+							text="09074265463"
 							href="tel:+2349074265463"
 							type="secondary"
 						/>
@@ -354,12 +368,22 @@ const ContactUs = () => {
 							<div>
 								<Button
 									className="rounded p-3 min-w-[90px] text-center"
-									disabled={loading || Object.values(formData).some(
-										(val, index) => val === ""
-									)}
+									disabled={
+										loading ||
+										Object.values(formData).some(
+											(val, index) => val === ""
+										)
+									}
 									onClick={submit}
 								>
-									{ loading? <FontAwesomeIcon icon={["fas","spinner"]} spin /> : "Send"}
+									{loading ? (
+										<FontAwesomeIcon
+											icon={["fas", "spinner"]}
+											spin
+										/>
+									) : (
+										"Send"
+									)}
 								</Button>
 							</div>
 						</form>
@@ -367,11 +391,14 @@ const ContactUs = () => {
 				</div>
 				<FollowUsLinks className="block md:hidden pt-6" />
 			</div>
-			
-			<SuccessModal open={openModal} onClose={()=>{
-				setOpenModal(false);
-				navigate("/");
-			}} />
+
+			<SuccessModal
+				open={openModal}
+				onClose={() => {
+					setOpenModal(false);
+					navigate("/");
+				}}
+			/>
 		</div>
 	);
 };
