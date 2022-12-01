4cb4cd172582713722603406c049ef3ba6b01b67
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-12-01 16:34:09 +0100

    Chore(BE-42): Minor fixes

--- server/models/Blog.js
+++ server/models/Blog.js
@@ -1,31 +1,31 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const BlogSchema = new Schema({
-	adminId: {
-		type: String,
-		required: true,
+const BlogSchema = new Schema(
+	{
+		adminId: {
+			type: String,
+			required: true,
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		content: {
+			type: String,
+			required: true,
+		},
+		likes: {
+			type: String,
+			default: 0,
+		},
+		createdAt: {
+			type: Date,
+			required: true,
+			default: Date.now,
+		},
 	},
-	title: {
-		type: String,
-		required: true,
-	},
-	content: {
-		type: String,
-		required: true,
-	},
-	likes: {
-		type: String,
-		default: 0,
-	},
-	lastEdited: {
-		type: Date,
-		required: true,
-		default: Date.now,
-	},
-	createdAt: {
-		type: Date,
-    required: true,
-		default: Date.now,
-	},
-});
+	{
+		timestamps: true,
+	}
+);
 module.exports = mongoose.model("Blog", BlogSchema);
