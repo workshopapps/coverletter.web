e40eb8dbb85f1b8824c2e0d3ce34e5738ef98529
jayGathu <jjgathu17@gmail.com>
2022-12-02 00:28:15 +0300

    fix: added success redirect

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const passport = require('passport')
-const jwt = require('jsonwebtoken')
-const auth = require('../middleware/authentication')
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+const auth = require("../middleware/authentication");
 const {
 	register,
 	updatePassword,
@@ -26,18 +26,28 @@ router.post("/forgotPassword", forgotPassword);
 
 router.post("/validateOTP", validateOTP);
 //GOOGLE auth routes
-router.get('/google', 
-  passport.authenticate('google', { scope : ['profile', 'email'] }));
- 
-router.get('/google/callback', 
-  passport.authenticate('google', { failureRedirect: 'coverly.hng.tech/signin' }),
-  function(req, res) {
-    const user = req.user
-    const token =  jwt.sign({googleID:user._id,name:user.name, email:user.email},process.env.JWT_SECRET,{expiresIn: "2h"})
-    
-   return res.status(200).json({user,token});
-  });
+router.get(
+	"/google",
+	passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
+router.get(
+	"/google/callback",
+	passport.authenticate("google", {
+		failureRedirect: "coverly.hng.tech/signin",
+		successRedirect: "https://coverly.hng.tech",
+	}),
+	function (req, res) {
+		const user = req.user;
+		const token = jwt.sign(
+			{ googleID: user._id, name: user.name, email: user.email },
+			process.env.JWT_SECRET,
+			{ expiresIn: "2h" }
+		);
+
+		return res.status(200).json({ user, token });
+	}
+);
 
 // All After login routes goes below PROTECT ROUTE
 router.use(protect);
