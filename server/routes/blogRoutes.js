bd37adb475b7764e394f9f09db2c7900e4c04552
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-12-01 13:25:19 +0100

    Chore(BE-42): Added authentication to blogRoute to create a blog post

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const { createPost } = require("../controllers/blogController");
+const auth = require("../middleware/authentication");
 
-router.post("/admin/blog/create", createPost);
+router.post("/admin/blog/create", auth, createPost);
 
 module.exports = router;
