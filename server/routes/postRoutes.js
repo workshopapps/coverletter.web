1f8aa0206b92dec64668541a684ea5a5d888ebac
Temitope Agbaje <agbajeinioluwa@gmail.com>
2022-12-02 15:42:34 +0100

    feat (BE-66) : Get all Posts for forums page end point

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const auth = require('../middleware/authentication')
+const auth = require("../middleware/authentication");
 
-const {createPost} = require('../controllers/postController')
+const { createPost } = require("../controllers/postController");
+const { getAllPosts } = require("../controllers/postController");
 const router = express.Router();
 
 // your routes here
-router.post('/createPost', auth, createPost)
+router.post("/createPost", auth, createPost);
+router.post("/getAllPost", getAllPosts);
 
-
-module.exports=router
+module.exports = router;
