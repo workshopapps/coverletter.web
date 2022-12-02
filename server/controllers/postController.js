b8a7f95e219b2caf5c714580977ea1040b9eaba5
Okeke Chigozie <81691598+bertinchigozie@users.noreply.github.com>
2022-12-02 13:53:52 +0100

    feat(BE65): added return to the creatPost

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -7,7 +7,7 @@ const createPost = async (req, res) => {
 	if (!req.body.title || !req.body.content)
 		throw new BadRequestError("Cannot create post");
 	var post = await Post.create(req.body);
-	res.status(StatusCodes.CREATED).json({ post });
+	return res.status(StatusCodes.CREATED).json({ post });
 };
 
 module.exports = {
