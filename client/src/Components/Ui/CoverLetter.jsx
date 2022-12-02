1b65ac2622621c720dacb9580906902bd24e418f
HaroldObasi <haroldobasi2k16@gmail.com>
2022-12-02 13:25:24 +0100

    minor style changes

--- client/src/Components/Ui/CoverLetter.jsx
+++ client/src/Components/Ui/CoverLetter.jsx
@@ -4,17 +4,17 @@ const CoverLetter = () => {
 	const { coverLetter: data, userData } = useGlobalContext();
 
 	return (
-		<div className="w-[255px] h-[362px] sm:w-[452px] sm:h-[640px] bg-textWhite border-grey300 target-2 border-2 pt-7 px-7 rounded-lg">
+		<div className="w-[255px] h-[362px] sm:w-[452px] coverletter-container sm:h-[640px] bg-textWhite border-grey300 target-2 border-2 pt-7 px-7 rounded-lg">
 			<div id="coverletter-target">
 				<div className="flex w-full justify-between items-end">
 					<p className="w-[50%] text-[7px] sm:text-xs flex text-grey300">
 						{userData.address} {userData.email}
 					</p>
-					<p className="w-[50%] text-right text-[14px] sm:text-2xl font-black ">
+					<p className="w-[50%] text-right text-[14px] sm:text-2xl font-black">
 						{userData.name}
 					</p>
 				</div>
-				<hr className="w-full h-[5px] md:h-[12px] outline-none h-2 mt-2 bg-primaryDeep border-none" />
+				<hr className="w-full md:h-[12px] outline-none h-2 mt-2 bg-primaryDeep border-none" />
 				<div className="mt-5 sm:mt-10 w-full">
 					<p className="w-[40%] text-[7px] sm:text-xs flex">
 						{/* {data.company_address} */}
@@ -36,10 +36,6 @@ const CoverLetter = () => {
 							__html: data.cover_letter,
 						}}
 					></p>
-					{/* <br />
-				<p>Best regards,</p>
-				<br />
-				<p>Sam Johnson</p> */}
 				</div>
 			</div>
 		</div>
