2476678b287c2efc0178c8b10b6e51e04f9ddced
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-12-03 23:08:37 +0100

    Chore(BE-81): Updated customer stories model

--- server/models/CustomerStories.js
+++ server/models/CustomerStories.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const CustomerStoriesSchema = new Schema(
 	{
-		// adminId: {
-		// 	type: Schema.Types.ObjectId,
-		// 	required: true,
-		// },
-		// userId: {
-		// 	type: Schema.Types.ObjectId,
-		// 	required: true,
-		// },
+		adminId: {
+			type: Schema.Types.ObjectId,
+			required: true,
+		},
+		userId: {
+			type: Schema.Types.ObjectId,
+			required: true,
+		},
 		title: {
 			type: String,
 			required: true,
