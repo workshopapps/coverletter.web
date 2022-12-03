8b4a707e5c4c9da02cdd19b361c17e3c78f765fe
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-12-03 19:48:01 +0100

    Feat(BE-81): Created customer stories model

---
+++ server/models/CustomerStories.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const CustomerStoriesSchema = new Schema(
+	{
+		fullName: {
+			type: String,
+			required: true,
+		},
+		job: {
+			type: String,
+			required: true,
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		subTitle: {
+			type: String,
+			required: true,
+		},
+		introduction: {
+			type: String,
+			required: true,
+		},
+		challenge: [
+			{
+				body: {
+					type: String,
+					required: true,
+				},
+				comment: {
+					type: String,
+					required: true,
+				},
+			},
+		],
+		solution: [
+			{
+				body: {
+					type: String,
+					required: true,
+				},
+				comment: {
+					type: String,
+					required: true,
+				},
+			},
+		],
+		outcome: [
+			{
+				body: {
+					type: String,
+					required: true,
+				},
+				comment: {
+					type: String,
+					required: true,
+				},
+			},
+		],
+	},
+	{
+		timestamps: true,
+	}
+);
+module.exports = mongoose.model("CustomerStories", CustomerStoriesSchema);
