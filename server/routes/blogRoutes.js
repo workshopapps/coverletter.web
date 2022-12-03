087b6617c303154d4eeccc12b0783810199e4857
ikwuh jeffery enokela <ikwuhj@gmail.com>
2022-12-03 18:18:06 +0100

    fix blog route errors

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { createPost, getAllPosts, getOnePost } = require("../controllers/blogController");
-const auth = require("../middleware/authentication");
 
 const {
-	createPost,
 	deleteABlogPost,
 	searchPost,
 	getABlogPost,
 	updatePost,
+	createPost,
+	getAllPosts,
+	getOnePost,
 } = require("../controllers/blogController");
 const auth = require("../middleware/authentication");
 
 router.get("/blogs/search", searchPost);
 router.get("/blogs/:blogId", getABlogPost);
-router.get("/blog/", getAllPosts)
+router.get("/blog/", getAllPosts);
 router.post("/admin/blog/", createPost);
 router.delete("/admin/blog/:blogId", auth, deleteABlogPost);
 router.put("/admin/blogd/:id", updatePost);
