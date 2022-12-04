679806bfe534a4246f0d39baeb49f0b309f94dde
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-12-04 12:41:28 +0100

    Chore(BE-75): Setting up mongoose for the adminDashboard

--- server/routes/adminDashboard.js
+++ server/routes/adminDashboard.js
@@ -1,8 +1,12 @@
 const AdminBro = require("admin-bro");
 const AdminBroExpress = require("admin-bro-expressjs");
+const AdminBroMongoose = require("admin-bro-mongoose");
+
+const mongoose = require("mongoose");
+AdminBro.registerAdapter(AdminBroMongoose);
 
 const adminBro = new AdminBro({
-	databases: [], // We don’t have any resources connected yet.
+	databases: [mongoose], // We don’t have any resources connected yet.
 	rootPath: "/admin", // Path to the AdminJS dashboard.
 });
 // Build and use a router to handle AdminJS routes.
