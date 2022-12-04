fc0e8f11b44ef95039844e98903a442a69011768
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-12-04 12:51:50 +0100

    Chore(BE-75): Added comments for other devs to undestand

--- server/routes/adminDashboard.js
+++ server/routes/adminDashboard.js
@@ -1,20 +1,26 @@
 const AdminBro = require("admin-bro");
 const AdminBroExpress = require("admin-bro-expressjs");
 const AdminBroMongoose = require("admin-bro-mongoose");
-
 const mongoose = require("mongoose");
+
 AdminBro.registerAdapter(AdminBroMongoose);
 
 const adminBro = new AdminBro({
-	databases: [mongoose], // We don’t have any resources connected yet.
-	rootPath: "/admin", // Path to the AdminJS dashboard.
+	databases: [mongoose], 
+	rootPath: "/admin",
 });
+
+
+// Here is just dummy code for the admin details and the admin email and password which would be stored in the env file
 
 const ADMIN = {
 	email: process.env.ADMIN_EMAIL || "peter@gmail.com",
 	password: process.env.ADMIN_PASS || "qwerop",
 };
-// Build and use a router to handle AdminJS routes.
+
+
+// Here I am creating an authenticated build for the admin login so anybody cannot just login to the dashboard without an email address and password 
+
 const adminDashboard = AdminBroExpress.buildAuthenticatedRouter(adminBro, {
 	cookieName: process.env.ADMIN_COOKIE_NAME || "admin-bro",
 	cookiePassword:
