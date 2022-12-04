9c93e24787bd5bbefaeaf8fb17e32da56684f20f
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-12-04 12:49:18 +0100

    Feat(BE-75): Completed work on authentication of the admin

--- server/routes/adminDashboard.js
+++ server/routes/adminDashboard.js
@@ -9,9 +9,22 @@ const adminBro = new AdminBro({
 	databases: [mongoose], // We don’t have any resources connected yet.
 	rootPath: "/admin", // Path to the AdminJS dashboard.
 });
+
+const ADMIN = {
+	email: process.env.ADMIN_EMAIL || "peter@gmail.com",
+	password: process.env.ADMIN_PASS || "qwerop",
+};
 // Build and use a router to handle AdminJS routes.
 const adminDashboard = AdminBroExpress.buildAuthenticatedRouter(adminBro, {
 	cookieName: process.env.ADMIN_COOKIE_NAME || "admin-bro",
+	cookiePassword:
+		process.env.ADMIN_COOKIE_PASS || "super-secret-long-password-for-admin",
+	authenticate: async (email, password) => {
+		if (email === ADMIN.email && password === ADMIN.password) {
+			return ADMIN;
+		}
+		return null;
+	},
 });
 
 module.exports = adminDashboard;
