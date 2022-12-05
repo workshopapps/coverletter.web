e16c6d24985a8ddeed11ec4ee2037cea2bfda749
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-12-05 08:00:09 +0100

    Chore(BE-59): Uncommented blog controller

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -8,26 +8,23 @@ const Reply = require("../models/Reply");
 const Comment = require("../models/Comment");
 
 const createPost = async (req, res) => {
-	// console.log()
-	// console.log(file)
-	// console.log(req.public_id);
-	// // const { title, content } = req.body;
-	// // if (!title || !content)
-	// // 	return res.status(StatusCodes.NO_CONTENT).json({
-	// // 		message: "All Fields are required",
-	// // 	});
-	// // const admin = await Admin.findById(req.user.userId);
-	// // if (!mongoose.Types.ObjectId.isValid(req.user.userId) || !admin) {
-	// // 	throw new BadRequestError(
-	// // 		"This adminId is not valid or the admin does not exist in our database."
-	// // 	);
-	// // }
-	// // const post = new Blog({
-	// // 	title,
-	// // 	content,
-	// // 	userId: adminId,
-	// // });
-	// // await post.save();
+	const { title, content } = req.body;
+	if (!title || !content)
+		return res.status(StatusCodes.NO_CONTENT).json({
+			message: "All Fields are required",
+		});
+	const admin = await Admin.findById(req.user.userId);
+	if (!mongoose.Types.ObjectId.isValid(req.user.userId) || !admin) {
+		throw new BadRequestError(
+			"This adminId is not valid or the admin does not exist in our database."
+		);
+	}
+	const post = new Blog({
+		title,
+		content,
+		userId: adminId,
+	});
+	await post.save();
 
 	return res.status(StatusCodes.CREATED).json({
 		message: "Creation of blog post was successful.",
