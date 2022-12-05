7313354eb47dfe65d49cf485e5520d7cc10d5b45
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-12-05 12:19:25 +0100

    Chore(BE-59): Minor fix for admin dashboard

--- server/routes/adminDashboard.js
+++ server/routes/adminDashboard.js
@@ -10,25 +10,6 @@ const adminBro = new AdminBro({
 	databases: [mongoose],
 	rootPath: "/admin",
 });
-
-// Here is just dummy code for the admin details and the admin email and password which would be stored in the env file
-
-const ADMIN = {
-	email: "peter@gmail.com",
-	password: "qwerop",
-};
-
-// Here I am creating an authenticated build for the admin login so anybody cannot just login to the dashboard without an email address and password
-
-const adminDashboard = AdminBroExpress.buildAuthenticatedRouter(adminBro, {
-	cookieName: "admin-bro",
-	cookiePassword: "super-secret-long-password-for-admin",
-	authenticate: async (email, password) => {
-		if (email === ADMIN.email && password === ADMIN.password) {
-			return ADMIN;
-		}
-		return null;
-	},
-});
+const adminDashboard = AdminBroExpress.buildRouter(adminBro);
 
 module.exports = adminDashboard;
