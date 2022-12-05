80b25e798b5267c7ecf140f9c50c2eb6a9e63276
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-12-05 07:59:27 +0100

    Chore(BE-59): Updated image upload middleware

--- server/middleware/image.js
+++ server/middleware/image.js
@@ -15,9 +15,7 @@ const uploadImage = async (req, res, next) => {
 		req.result = result;
 		next();
 	} catch (err) {
-		// console.log(err);
 		throw new Error("Failed to upload the image");
-		// return res.status(400).send(err);
 	}
 };
 
@@ -26,9 +24,7 @@ const deleteImage = async (req, res, next) => {
 		const result = await cloudinary.uploader.destroy(req.body.public_id);
 		const { public_id } = req.public_id;
 		next();
-		// return res.status(202).send(result);
 	} catch (err) {
-		// res.status(409).send(err);
 		throw new Error("Failed to delete the image");
 	}
 };
