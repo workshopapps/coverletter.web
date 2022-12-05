612555a28b313af62e04d5df3554882a9162121c
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-12-05 10:59:46 +0100

    Chore(BE-59): Minor fix for admin dashboard

--- server/routes/adminDashboard.js
+++ server/routes/adminDashboard.js
@@ -21,9 +21,8 @@ const ADMIN = {
 // Here I am creating an authenticated build for the admin login so anybody cannot just login to the dashboard without an email address and password
 
 const adminDashboard = AdminBroExpress.buildAuthenticatedRouter(adminBro, {
-	cookieName: process.env.ADMIN_COOKIE_NAME || "admin-bro",
-	cookiePassword:
-		process.env.ADMIN_COOKIE_PASS || "super-secret-long-password-for-admin",
+	cookieName: "admin-bro",
+	cookiePassword: "super-secret-long-password-for-admin",
 	authenticate: async (email, password) => {
 		if (email === ADMIN.email && password === ADMIN.password) {
 			return ADMIN;
