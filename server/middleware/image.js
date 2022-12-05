22e4a4e897881c33f449e7102267eda41f19d1e3
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-12-05 04:20:36 +0100

    Chore(BE-59): Updated image middleware

--- server/middleware/image.js
+++ server/middleware/image.js
@@ -4,24 +4,39 @@ const upload = require("../utils/multer");
 const cloudinary = require("../utils/cloudinary");
 const { image } = require("../utils/cloudinary");
 
-imageRouter.post("/", upload.single("image"), async (req, res) => {
+// imageRouter.post("/", upload.single("image"), async (req, res) => {});
+
+// imageRouter.post("/delete", async (req, res) => {
+// 	try {
+// 		const result = await cloudinary.uploader.destroy(req.body.public_id);
+// 		return res.status(202).send(result);
+// 	} catch (err) {
+// 		res.status(409).send(err);
+// 	}
+// });
+
+const uploadImage = async (req, res, next) => {
 	try {
 		const result = await cloudinary.uploader.upload(req.file.path);
-		const { secure_url, public_id } = result;
-		return res.status(200).json({ secure_url, public_id });
+		const { secure_url, public_id } = req.public_id;
+		// return res.status(200).json({ secure_url, public_id });
+		next();
 	} catch (err) {
 		console.log(err);
-		return res.status(400).send(err);
+		throw new Error("Failed to upload the image");
+		// return res.status(400).send(err);
 	}
-});
+};
 
-imageRouter.post("/delete", async (req, res) => {
+const deleteImae = async (req, res, next) => {
 	try {
 		const result = await cloudinary.uploader.destroy(req.body.public_id);
-		return res.status(202).send(result);
+		const { public_id } = req.public_id;
+		next();
+		// return res.status(202).send(result);
 	} catch (err) {
-		res.status(409).send(err);
+		// res.status(409).send(err);
+		throw new Error("Failed to delete the image");
 	}
-});
-
+};
 module.exports = imageRouter;
