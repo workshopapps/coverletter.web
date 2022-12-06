149018a6cdbf2a9fd56c0be7d533cd9c605bb5bc
oyinkansola <adebayoalameen88@gmail.com>
2022-12-06 21:08:46 +0100

    Feat: Integrated an API on forum page

--- client/src/pages/forum/Post.jsx
+++ client/src/pages/forum/Post.jsx
@@ -1,36 +1,100 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 const Post = () => {
-  return <>
-    <main className='bg-[#f2f2f7] mx-0 my-0 py-5 px-5 md:px-16'>
-        <div className='text-2xl md:text-[40px] font-bold mb-10 md:mb-5'>
-            <p>Create a new post</p>
-        </div>
-        <div className='flex gap-3 items-center'>
-            <div>
-                <img className='rounded-full w-12 sm:w-16 object-cover' src="../forum-images/reply/r7.png" alt="" />
-            </div>
-            <div>
-                <div className='capitalize text-base md:text-2xl font-semibold'>
-                    <p>Kreativ Mind</p>
-                </div>
-                <div className='flex gap-5 text-five text-sm md:text-base'>
-                    <p className='text-[#bababa]'>November 2, 2022</p>
-                </div>
-            </div>
-        </div>
-        <form className='mt-5' method='post'>
-            <div className='flex flex-col gap-3 h-[400px] bg-[#fcfcfc]'>
-                <input className='border-none outline-none appearance-none bg-opacity-0 px-6 pt-3' type="text" placeholder='Title' />
-                <div className='w-full h-px bg-[#CAD0DD]'></div>
-                <textarea rows='14' className='appearance-none outline-none resize-none bg-opacity-0 px-6' name="" id="" placeholder='Type text here' ></textarea>
-            </div>
-            <div className='w-full text-right mt-3'>
-                <button type="submit" className='btn btnPrimary w-[170px] text-base font-bold'>Post</button>
-            </div>
-        </form>
-    </main>
-  </>
-}
+	const [title, setTitle] = useState("");
+	const [contents, setContents] = useState("");
 
-export default Post
+	const submitHandler = (e) => {
+		e.preventDefault();
+
+		const createPost = async () => {
+			const formData = new FormData();
+			formData.append("title", title);
+			formData.append("content", contents);
+			console.log([...formData]);
+
+
+           const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization" : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MzhmNmE4MTczMmQwYzBkZTM3N2RjN2UiLCJuYW1lIjoiQWRlYmF5byBveWlua2Fuc29sYSAiLCJlbWFpbCI6ImFkZWJheW9hbGFtZWVuMkBnbWFpbC5jb20iLCJzdGF0dXMiOiJBY3RpdmUiLCJpYXQiOjE2NzAzNTExNTgsImV4cCI6MTY3MDM2OTE1OH0.yI4KfHIzbjkH92zGX3uz_LvDbOmgqgES7JzVWdoiB9c",
+                }
+            }
+
+			try {
+				const res = await axios.post(
+					`https://api.coverly.hng.tech/api/v1/forum/createPost`,
+                    formData,
+                    config
+					
+				);
+				console.log(res);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		createPost();
+	};
+	return (
+		<>
+			<main className="bg-[#f2f2f7] mx-0 my-0 py-5 px-5 md:px-16">
+				<div className="text-2xl md:text-[40px] font-bold mb-10 md:mb-5">
+					<p>Create a new post</p>
+				</div>
+				<div className="flex gap-3 items-center">
+					<div>
+						<img
+							className="rounded-full w-12 sm:w-16 object-cover"
+							src="../forum-images/reply/r7.png"
+							alt=""
+						/>
+					</div>
+					<div>
+						<div className="capitalize text-base md:text-2xl font-semibold">
+							<p>Kreativ Mind</p>
+						</div>
+						<div className="flex gap-5 text-five text-sm md:text-base">
+							<p className="text-[#bababa]">November 2, 2022</p>
+						</div>
+					</div>
+				</div>
+				<form
+					className="mt-5"
+					method="post"
+					encType="multipart/form-data"
+					onSubmit={submitHandler}
+				>
+					<div className="flex flex-col gap-3 h-[400px] bg-[#fcfcfc]">
+						<input
+							className="border-none outline-none bg-opacity-0 px-6 pt-3"
+							type="text"
+							placeholder="Title"
+							value={title}
+							onChange={(e) => setTitle(e.target.value)}
+						/>
+						<div className="w-full h-px bg-[#CAD0DD]"></div>
+						<textarea
+							rows="14"
+							className="appearance-none outline-none resize-none bg-opacity-0 px-6"
+							id=""
+							placeholder="Type text here"
+							value={contents}
+							onChange={(e) => setContents(e.target.value)}
+						></textarea>
+					</div>
+					<div className="w-full text-right mt-3">
+						<button
+							type="submit"
+							className="btn btnPrimary w-[170px] text-base font-bold"
+						>
+							Post Now
+						</button>
+					</div>
+				</form>
+			</main>
+		</>
+	);
+};
+
+export default Post;
