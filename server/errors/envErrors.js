3ab7f4c75f9ce8fd958f35e18df29a30d5f77d44
Tommy, Ubongabasi <tommyubongabasijohn@gmail.com>
2022-12-08 18:39:59 +0100

    environment based error handlers

---
+++ server/errors/envErrors.js
@@ -0,0 +1,32 @@
+const devErr = (err, req, res) => {
+    if (req.originalUrl.startsWith('/api')) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+      });
+    }
+};
+
+
+const prodErr = (err, req, res) => {
+    if (req.originalUrl.startsWith('/api')) {
+      //Predicted error. Send descriptive message to client
+      if (err.isPredictable) {
+        return res.status(err.statusCode).json({
+          status: err.status,
+          message: err.message
+        });
+      }
+      //Need to think about error logging to ensure devs have a way of tracking such
+      return res.status(500).json({
+        status: 'error',
+        message: 'Something went very wrong!'
+      });
+    }
+};
+
+module.exports = {
+    devErr, prodErr
+}
