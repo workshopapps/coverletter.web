9b1920faa95d132402703bab1ecc55052b792942
oyinkansola <adebayoalameen88@gmail.com>
2022-12-08 02:34:24 +0100

    Fix

--- client/src/Components/input/InputData.js
+++ client/src/Components/input/InputData.js
@@ -845,7 +845,7 @@ function InputData() {
 			</main>
 			{isLoading && (
 				<div className="bg-overlay fixed top-0 left-0 bottom-0 z-50 w-full flex justify-center items-center ">
-					<div className=" bg-textWhite w-[75%] md:w-[70%]  lg:w-[50%] rounded-md h-[280px] md:h-[400px] flex flex-col justify-center items-center gap-[20px]">
+					<div className=" bg-textWhite w-[80%] md:w-[70%]  lg:w-[50%] rounded-md h-[376px] md:h-[512px] flex flex-col justify-center items-center gap-[20px]">
 						<h3 className="text-textBody text-center text-[16px]">
 							{percentage < 78
 								? "Extracting your details..."
