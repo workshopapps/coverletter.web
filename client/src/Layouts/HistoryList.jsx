b12858de00eaa13de6c772e0ba51c7aa952c9a99
Isaac Idibia <idibiaisaac3@gmail.com>
2022-12-10 23:44:46 +0100

    feat: no history page

--- client/src/Layouts/HistoryList.jsx
+++ client/src/Layouts/HistoryList.jsx
@@ -2,20 +2,28 @@ import HistoryItem from "./HistoryItem";
 import cvHistory from "../Assets/cvHistory.png";
 import Button from "../Components/Ui/Button";
 import { Link } from "react-router-dom";
+import { ReactComponent as NotFound } from "../Assets/not-found-history.svg";
 
 const HistoryList = (props) => {
 	if (props.Items.length < 1) {
 		return (
-			<div className="w-full h-full pb-[150px] pt-[50px] flex flex-col justify-center items-center text-center">
-				<p className="sm:text-[1.5rem]">
-					You have no cover letter saved
+			<div className="flex flex-col justify-center items-center text-center">
+				<NotFound />
+				<p className="sm:text-[1.5rem] text-grey800 text-2xl font-bold">
+					No Letter History Found
+					<br />
+					<span className="font-normal text-base text-grey400 w-[256px]">
+						You have not generated any
+						<br />
+						cover letters
+					</span>
 				</p>
-				<Link to="/">
+				<Link to="/generate">
 					<Button
 						className={
-							"btn px-4 md:px-7 btnPrimary mt-6 text-sm sm:text-md"
+							"btn px-4 md:px-7 btnPrimary mt-6 text-sm sm:text-md md:w-[400px] h-12 md:text-base"
 						}
-						children={"Create a Cover Letter"}
+						children={"Generate cover letter"}
 						disabled={false}
 					></Button>
 				</Link>
--- client/src/pages/History.jsx
+++ client/src/pages/History.jsx
@@ -103,20 +103,7 @@ const History = () => {
 	return (
 		<main className="bg-background py-10">
 			<div className="max-w-screen-2xl m-auto px-5">
-				<div className="mb-16 max-[576px]:mb-8">
-					<h1 className="text-grey800 text-[56px] leading-[4.5rem] max-[768px]:text-2xl">
-						History
-					</h1>
-					<p className="text-grey400 text-2xl mt-1 max-[576px]:text-base">
-						Cover Letters
-					</p>
-				</div>
-
-				<div className="bg-white px-12 py-9">
-					<p className="text-grey800 font-semibold text-[40px] leading-[3rem] mb-8 max-[768px]:text-xl">
-						All Cover Letters
-					</p>
-
+				<div className="px-12 py-9">
 					<HistoryList
 						Items={clList}
 						deleteCoverLetter={deleteCoverLetter}
