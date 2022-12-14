099a27ac245533765b457784f301e7bcb8963138
jayGathu <jjgathu17@gmail.com>
2022-12-14 20:34:35 +0300

    fix: save coverletter with google id

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -14,9 +14,9 @@ const auth = async (req, res, next) => {
 		const payload = jwt.verify(token, process.env.JWT_SECRET);
 		// attach the user to the job routes
 		req.user = {
-			userId: payload.userId,
-			name: payload.name,
-			email: payload.email,
+			userId: payload.userId || payload.user._id,
+			name: payload.name || payload.user.name,
+			email: payload.email || payload.user.email,
 		};
 		next();
 	} catch (error) {
