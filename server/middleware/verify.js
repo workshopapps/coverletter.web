f699832bb4960f37089637b7c6ce0aeafe7e1ff8
jayGathu <jjgathu17@gmail.com>
2022-12-15 06:56:45 +0300

    removed log

--- server/middleware/verify.js
+++ server/middleware/verify.js
@@ -2,7 +2,6 @@ const User = require("../models/User");
 const { UnauthenticatedError } = require("../errors");
 
 const verify = async (req, res, next) => {
-	console.log(req.user.login, req.user.userId);
 	if (req.user.login === "Signin") {
 		const user = await User.findOne({ email: req.user.email });
 		console.log(user);
