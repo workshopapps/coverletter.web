f42ef85c1ef0e37fe08f349d02541ceb18c7e2e2
jayGathu <jjgathu17@gmail.com>
2022-12-16 16:02:27 +0300

    remove log

--- server/middleware/verify.js
+++ server/middleware/verify.js
@@ -4,7 +4,7 @@ const { UnauthenticatedError } = require("../errors");
 const verify = async (req, res, next) => {
 	if (req.user.login === "Signin") {
 		const user = await User.findOne({ email: req.user.email });
-		console.log(user);
+
 		if (user.status === "Pending") {
 			throw new UnauthenticatedError("User has not been verified");
 		}
